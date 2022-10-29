6ed03f7cdbf0da97cb8fb66507292fe5dadb3a69
jinkyumpark <jinpark1025@gmail.com>
2022-10-30 08:07:29 +0900

    add book models
    add book model and related models (author, book_category, nationality)

---
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/book/Author.java
@@ -0,0 +1,23 @@
+package com.jinkyumpark.bookitout.book;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "Author")
+@Table(name = "author")
+public class Author {
+    @Id
+    @SequenceGenerator(name = "author_seq", sequenceName = "author_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_seq")
+    @Column(name = "id")
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "nationality_id", nullable = false, referencedColumnName = "id", foreignKey = @ForeignKey(name = "author_nationality_fk"))
+    private Nationality nationality;
+
+    @Column(name = "birthdate", nullable = true)
+    private LocalDateTime birthDate;
+}
