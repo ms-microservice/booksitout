7634ee6caa0cf42dd4741d7c18a409f77daa70c8
jinkyumpark <jinpark1025@gmail.com>
2022-10-29 13:34:21 +0900

    add project
    add project with spring initilizer with spring-boot-starter-web dependency

---
+++ BookItOut/src/test/java/com/jinkyumpark/bookitout/BookItOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinkyumpark.bookitout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookItOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
