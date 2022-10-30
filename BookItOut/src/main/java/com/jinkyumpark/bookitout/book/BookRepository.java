06686b68b6fe6ba56db311438838c38a5f77f18e
jinkyumpark <jinpark1025@gmail.com>
2022-10-30 14:16:42 +0900

    remove server app dir

--- BookItOut/src/main/java/com/jinkyumpark/bookitout/book/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jinkyumpark.bookitout.book;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {}
