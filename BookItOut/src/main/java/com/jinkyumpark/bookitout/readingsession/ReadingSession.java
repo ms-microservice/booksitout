da51822a4373e22d5b052772b0ef6b986994206b
jinkyumpark <jinpark1025@gmail.com>
2022-11-02 07:42:21 +0900

    add reading session model
    id, user(fk), book(fk), startPage, endPage, startTime, endTime

--- BookItOut/src/main/java/com/jinkyumpark/bookitout/readingsession/ReadingSession.java
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/readingsession/ReadingSession.java
@@ -1,2 +1,40 @@
-package com.jinkyumpark.bookitout.readingsession;public class ReadingSession {
+package com.jinkyumpark.bookitout.readingsession;
+
+import com.jinkyumpark.bookitout.user.AppUser;
+import com.jinkyumpark.bookitout.userbook.UserBook;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "readingsession")
+@Table(name = "ReadingSession")
+public class ReadingSession {
+    @Id
+    @SequenceGenerator(name = "reading_session_seq", sequenceName = "reading_session_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reading_session_seq")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "reading_session_user_id", referencedColumnName = "id", updatable = false, foreignKey = @ForeignKey(name = "reading_session_user_fk"))
+    private AppUser appUser;
+
+    @ManyToOne
+    @JoinColumn(name = "reading_session_book_id", referencedColumnName = "id", updatable = false, foreignKey = @ForeignKey(name = "reading_session_book_fk"))
+    private UserBook userBook;
+
+    @Column(name = "start_page")
+    private Integer startPage;
+
+    @Column(name = "end_page")
+    private Integer endPage;
+
+    @Generated(GenerationTime.INSERT)
+    @Column(name = "start_time", updatable = false, insertable = false, nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
 }
