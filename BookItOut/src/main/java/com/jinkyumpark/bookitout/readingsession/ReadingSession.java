542b449a35a0a9031eb7d0571011f243482783e3
jinkyumpark <jinpark1025@gmail.com>
2022-11-02 07:36:08 +0900

    add userbook model, add BookSource enum
    userbook : book(fk), user(fk), rating, review, endPage, currentPage, source

---
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/readingsession/ReadingSession.java
@@ -0,0 +1,2 @@
+package com.jinkyumpark.bookitout.readingsession;public class ReadingSession {
+}
---
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/userbook/BookSource.java
@@ -0,0 +1,14 @@
+package com.jinkyumpark.bookitout.userbook;
+
+public enum BookSource {
+    BOUGHT_BOOKSTORE,
+    BOUGHT_ONLINE,
+    BOUGHT_USED,
+
+    BORROW_LIBRARY,
+    BORROW_STORE,
+    BORROW_FRIENDS,
+
+    EBOOK_BOUGHT,
+    EBOOK_SUBSCRIPTION,
+}
