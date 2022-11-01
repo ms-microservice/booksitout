8634c07fe03b72d305d781d27ffe940c1b1c9206
jinkyumpark <jinpark1025@gmail.com>
2022-11-02 07:20:12 +0900

    add language model
    id, name(unique), icon, spokenPopulation

---
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/language/Language.java
@@ -0,0 +1,23 @@
+package com.jinkyumpark.bookitout.language;
+
+import javax.persistence.*;
+
+@Entity(name = "language")
+@Table(name = "Language", uniqueConstraints = {
+        @UniqueConstraint(name = "language_name_unique", columnNames = "name")
+})
+public class Language {
+    @Id
+    @SequenceGenerator(name = "language_seq", sequenceName = "language_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "language_seq")
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "icon", nullable = false)
+    private String icon;
+
+    @Column(name = "spoken_population")
+    private Integer spokenPopulation;
+}
