c4f47d5c410e7d4255a9d56a84c962d4bf1bfcd6
jinkyumpark <jinpark1025@gmail.com>
2022-11-16 18:16:42 +0900

    front: cleanup App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,52 @@
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import Topnav from './common/Topnav';
-import Summary from './summary/Summary';
-import Introduction from './introduction/Introduction';
-import Login from './user/Login';
-import Join from './user/Join';
+import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+// common
+import Topnav from './common/Topnav'
+import AddButton from './common/AddButton'
+// user
+import Login from './user/Login'
+import Join from './user/Join'
+import Settings from './user/Settings'
+// book
+import BookList from './book/BookList'
+import BookDetail from './book/BookDetail'
+import BookAddForm from './book/BookAddForm'
+// others
+import Summary from './summary/Summary'
+import Introduction from './introduction/Introduction'
 
 function App() {
-    const [token, setToken] = useState(localStorage.getItem('login-token'));
+    const [token, setToken] = useState(localStorage.getItem('login-token'))
 
     return (
         <div className='App'>
             <Topnav token={token} />
 
-            <div
-                className='div'
-                style={{
-                    marginBottom: '75px',
-                }}
-            ></div>
+            <div style={{marginBottom: '80px'}}/>
 
             <Routes>
-                <Route path='/' element={<Summary />} />
+                <Route path='/' element={<Summary token={token} />} />
                 <Route path='/introduction' element={<Introduction />} />
+                <Route path='/qna' element={<></>}/>
+                <Route path='/faq' element={<></>}/>
 
                 <Route path='/login' element={<Login setToken={setToken} />} />
                 <Route path='/join' element={<Join />} />
-                <Route path='/settings' element={<></>} />
+                <Route path='/settings' element={<Settings />} />
 
-                <Route path='/book/:range' element={<></>} />
-                <Route path='/book/detail/:id' element={<></>} />
-                <Route path='/review' element={<></>} />
+                <Route path='/book/:range' element={<BookList token={token} />}/>
+                <Route path='/book/detail/:id' element={<BookDetail token={token}/>} />
+                <Route path='book/add' element={<BookAddForm token={token}/>} />
+                <Route path='/book/edit' element={<></>}/>
                 <Route path='/statistics' element={<></>} />
                 <Route path='/reading' element={<></>} />
 
                 <Route path='/search/:key' element={<></>} />
             </Routes>
+
+            <AddButton />
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
