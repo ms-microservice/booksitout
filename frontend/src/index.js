b774cc1ed3f81ef1ffce30440c4669832aaf7858
jinkyumpark <jinpark1025@gmail.com>
2022-11-19 22:24:02 +0900

    front: index.js: cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
+import './resources/css/index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import './resources/css/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <Router basename={process.env.PUBLIC_URL}>
-        <App />
-    </Router>
-);
+	<Router basename={process.env.PUBLIC_URL} refresh>
+		<App />
+	</Router>
+)
