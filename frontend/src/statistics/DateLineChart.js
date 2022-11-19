b7fe91a8b54893e1134d7e7f5c8c67e105afbf48
jinkyumpark <jinpark1025@gmail.com>
2022-11-19 22:25:02 +0900

    front: DateLineChart

--- frontend/src/statistics/DateLineChart.js
+++ frontend/src/statistics/DateLineChart.js
@@ -1,61 +1,37 @@
-import React from 'react';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import React from 'react'
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
+import { Line } from 'react-chartjs-2'
 
 const DateLineChart = ({ startDate, endDate = Date.now(), data }) => {
-    ChartJS.register(
-        CategoryScale,
-        LinearScale,
-        PointElement,
-        LineElement,
-        Title,
-        Tooltip,
-        Legend
-    );
+	ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
-    return (
-        <Line
-            options={{
-                plugins: {
-                    legend: {
-                        display: false,
-                    },
-                },
-            }}
-            data={{
-                labels: [
-                    ...Array.from(
-                        {
-                            length: 14,
-                        },
-                        (_, i) => {
-                            let day = new Date(startDate).setDate(
-                                new Date(startDate).getDate() + i
-                            );
-                            return new Date(day).getDate() + '일';
-                        }
-                    ),
-                ],
+	return (
+		<Line
+			options={{
+				plugins: {
+					legend: {
+						display: false,
+					},
+				},
+			}}
+			data={{
+				labels: [
+					...Array.from({ length: 14 }, (_, i) => {
+						let day = new Date(startDate).setDate(new Date(startDate).getDate() + i + 1)
+						return new Date(day).getDate() + '일'
+					}),
+				],
 
-                datasets: [
-                    {
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: data,
-                    },
-                ],
-            }}
-        />
-    );
-};
+				datasets: [
+					{
+						backgroundColor: 'rgb(255, 99, 132)',
+						borderColor: 'rgb(255, 99, 132)',
+						data: data,
+					},
+				],
+			}}
+		/>
+	)
+}
 
-export default DateLineChart;
+export default DateLineChart
