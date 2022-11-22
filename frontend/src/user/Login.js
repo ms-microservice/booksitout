278586a9a5ce44c9fa4033fe54f9760be93ff8af
jinkyumpark <jinpark1025@gmail.com>
2022-11-22 15:27:34 +0900

    front : add introduction, qna, faq link to Login.js

--- frontend/src/user/Login.js
+++ frontend/src/user/Login.js
@@ -4,7 +4,9 @@ import { Card, Form, Button } from 'react-bootstrap'
 
 const Login = (props) => {
 	const LOGIN_API_URL = `http://localhost/login`
-
+	const INTRODUCTION_URL = `/introduction`
+	const FAQ_URL = `/faq`
+	const QNA_URL = `/qna`
 	const EMAIL_PLACEHOLDER_MESSAGE = `이메일을 입력해 주세요`
 	const PASSWORD_PLACEHOLDER_MESSAGE = `비밀번호를 입력해 주세요`
 
@@ -57,73 +59,114 @@ const Login = (props) => {
 
 	return (
 		<div className='container mt-5'>
-			<div className='row justify-content-center'>
-				<Card className='col-12 col-lg-6 text-center'>
-					<Card.Body>
-						<h1>📗 로그인</h1>
+			<div className='row row-eq-height justify-content-center'>
+				<div className='col-12 col-lg-7 mb-4'>
+					<Card className='text-center'>
+						<Card.Body>
+							<h1>📗 로그인</h1>
 
-						<Form onSubmit={handleLogin}>
-							<Form.Group class='row mt-3'>
-								<div className='col-2'>
-									<label class='col-form-label text-start'>이메일</label>
-								</div>
+							<Form onSubmit={handleLogin}>
+								<Form.Group class='row mt-3'>
+									<div className='col-2'>
+										<label class='col-form-label text-start'>이메일</label>
+									</div>
 
-								<div class='col-10'>
-									<input
-										type='email'
-										class='form-control'
-										placeholder={EMAIL_PLACEHOLDER_MESSAGE}
-										onChange={(e) => setEmail(e.target.value)}
-										required
-									/>
-								</div>
-							</Form.Group>
+									<div class='col-10'>
+										<input
+											type='email'
+											class='form-control'
+											placeholder={EMAIL_PLACEHOLDER_MESSAGE}
+											onChange={(e) => setEmail(e.target.value)}
+											required
+										/>
+									</div>
+								</Form.Group>
 
-							<Form.Group class='row mt-3'>
-								<div className='col-2'>
-									<label class='text-start'>비밀번호</label>
-								</div>
+								<Form.Group class='row mt-3'>
+									<div className='col-2'>
+										<label class='text-start'>비밀번호</label>
+									</div>
 
-								<div class='col-10'>
-									<input
-										type='password'
-										class='form-control'
-										placeholder={PASSWORD_PLACEHOLDER_MESSAGE}
-										onChange={(e) => setPassword(e.target.value)}
-										required
-									/>
-								</div>
-							</Form.Group>
+									<div class='col-10'>
+										<input
+											type='password'
+											class='form-control'
+											placeholder={PASSWORD_PLACEHOLDER_MESSAGE}
+											onChange={(e) => setPassword(e.target.value)}
+											required
+										/>
+									</div>
+								</Form.Group>
 
-							<div class='form-check mt-3 ms-3 text-start'>
-								<label
-									onClick={() => {
-										setStayLogin(!stayLogin)
-									}}>
-									<label class='form-check-label' for='stay-login'>
-										로그인 유지하기
+								<div class='form-check mt-3 ms-3 text-start'>
+									<label
+										onClick={() => {
+											setStayLogin(!stayLogin)
+										}}>
+										<label class='form-check-label' for='stay-login'>
+											로그인 유지하기
+										</label>
+
+										<input type='checkbox' class='form-check-input' checked={stayLogin} value={stayLogin} />
 									</label>
-
-									<input type='checkbox' class='form-check-input' checked={stayLogin} value={stayLogin} />
-								</label>
-							</div>
-
-							<div className='row justify-content-center mt-3'>
-								<div className='col-4'>
-									<Button varnt='success' type='submit' className='w-100'>
-										로그인
-									</Button>
 								</div>
 
-								<div className='col-4'>
-									<Button variant='danger' className='w-100' href='join'>
-										회원가입
-									</Button>
+								<div className='row justify-content-center mt-3'>
+									<div className='col-4'>
+										<Button varnt='success' type='submit' className='w-100'>
+											로그인
+										</Button>
+									</div>
+
+									<div className='col-4'>
+										<Button variant='danger' className='w-100' href='join'>
+											회원가입
+										</Button>
+									</div>
+								</div>
+							</Form>
+						</Card.Body>
+					</Card>
+				</div>
+
+				<div className='col-12 col-lg-6 mt-4'>
+					<a href={INTRODUCTION_URL} className='text-decoration-none text-black h-100'>
+						<Card className='h-100'>
+							<Card.Body className='text-center'>
+								<h4>책-it-out이 뭐 하는 곳인지 궁금하신가요?</h4>
+
+								<h6 className='mt-5 mb-4'>책 읽는 모든 이들을 위한 곳, 책-it-out을 더 알고 싶으면 여기를 클릭해 주세요</h6>
+							</Card.Body>
+						</Card>
+					</a>
+				</div>
+
+				<div className='col-12 col-lg-6 mt-4'>
+					<Card className='h-100'>
+						<Card.Body className='text-center'>
+							<h4 className='text-center'>책-it-out에 관한 질문 한 스푼</h4>
+
+							<div className='row mt-3'>
+								<div className='col-12 col-md-6'>
+									<a href={FAQ_URL} className='text-decoration-none text-black'>
+										<Card>
+											<Card.Header>FAQ</Card.Header>
+											<Card.Body>자주 하시는 질문을 모아 봤어요</Card.Body>
+										</Card>
+									</a>
+								</div>
+								<div className='col-12 col-md-6'>
+									<a href={QNA_URL} className='text-decoration-none text-black'>
+										<Card>
+											<Card.Header>QNA</Card.Header>
+											<Card.Body>직접 질문하실 수 있어요</Card.Body>
+										</Card>
+									</a>
 								</div>
 							</div>
-						</Form>
-					</Card.Body>
-				</Card>
+						</Card.Body>
+					</Card>
+				</div>
 			</div>
 		</div>
 	)
