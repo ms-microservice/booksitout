32b576d674f4fc30eceaa05274c1514aa91b04be
jinkyumpark <jinpark1025@gmail.com>
2022-11-23 13:42:00 +0900

    add memo repository

---
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/memo/MemoRepository.java
@@ -0,0 +1,7 @@
+package com.jinkyumpark.bookitout.memo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemoRepository extends JpaRepository<Memo, Long> {}
