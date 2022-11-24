ff26bf0fa0b62a01219719a9a0218fbb3e18fc1a
jinkyumpark <jinpark1025@gmail.com>
2022-11-24 21:35:56 +0900

    front : Timer.js : fix bug : time improperly shown when hour exceed 1

--- frontend/src/book/reading/Timer.js
+++ frontend/src/book/reading/Timer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Timer = ({ time }) => {
 	return (
 		<h1>
-			{Math.floor(time / 60 / 60)} : {Math.floor(time / 60)} : {time % 60}
+			{Math.floor((time / 60 / 60) % (60 * 60))} : {Math.floor(time / 60) % 60} : {time % 60}
 		</h1>
 	)
 }
