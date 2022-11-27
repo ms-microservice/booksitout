51ef4e872e1704c73ff13e50bfa53457dfc0d10e
jinkyumpark <jinpark1025@gmail.com>
2022-11-28 02:27:25 +0900

    rename Yearly.java to YearStatistics.java

--- BookItOut/src/main/java/com/jinkyumpark/bookitout/app/statistics/model/YearStatistics.java
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/app/statistics/model/YearStatistics.java
@@ -1,11 +1,11 @@
-package com.jinkyumpark.bookitout.statistics.response;
+package com.jinkyumpark.bookitout.app.statistics.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class Yearly {
+public class YearStatistics {
     private Integer totalReadTime;
     private Integer totalReadBookCount;
     private Double averageStar;
