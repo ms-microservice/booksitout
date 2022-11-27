c787168e1a7a1f31809128c997b45f6d54173af4
jinkyumpark <jinpark1025@gmail.com>
2022-11-28 02:35:51 +0900

    StatisticsRepository.java : add @Query

--- BookItOut/src/main/java/com/jinkyumpark/bookitout/app/statistics/StatisticsRepository.java
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/app/statistics/StatisticsRepository.java
@@ -10,7 +10,12 @@ import java.util.Optional;
 
 @Repository
 public interface StatisticsRepository extends JpaRepository<MonthStatistics, Long> {
+    @Query("select m from month_statistics m where m.appUser.appUserId = ?1 and m.year = ?2 and m.month = ?3")
     Optional<MonthStatistics> findByAppUser_AppUserIdAndYearIsAndMonthIs(Long AppUserId, Integer year, Integer month);
+
+    @Query("select m from month_statistics m where m.appUser.appUserId = ?1 and m.year = ?2")
     List<MonthStatistics> findAllByAppUser_AppUserIdAndYearIs(Long appUserId, Integer year);
+
+    @Query("select m from month_statistics m where m.appUser.appUserId = ?1 and m.year = ?2 and m.month = ?3")
     Optional<MonthStatistics> findByAppUser_AppUserIdAndYearAndMonth(Long appUserId, Integer year, Integer month);
 }
