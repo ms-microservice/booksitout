f011a6a904f68fbdfee08b6b90f2d2e275128c0a
jinkyumpark <jinpark1025@gmail.com>
2022-11-28 02:34:46 +0900

    ReadingSessionIsInProgressException.java : make bookId final

--- BookItOut/src/main/java/com/jinkyumpark/bookitout/exception/custom/ReadingSessionIsInProgressException.java
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/exception/custom/ReadingSessionIsInProgressException.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ReadingSessionIsInProgressException extends RuntimeException {
     private final Integer status = HttpStatus.CONTINUE.value();
     private String message = "아직 진행중이신 독서활동이 있어요. 전의 독서활동을 먼저 끝내 주세요";
-    private Long bookId;
+    private final Long bookId;
 
     public ReadingSessionIsInProgressException(Long bookId, String message) {
         this.bookId = bookId;
