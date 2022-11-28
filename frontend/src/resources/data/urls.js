6368fc57ac204b5affd8feba7ea17fdf78bf4978
jinkyumpark <jinpark1025@gmail.com>
2022-11-28 18:46:06 +0900

    front : add urls

---
+++ frontend/src/resources/data/urls.js
@@ -0,0 +1,23 @@
+const API_BASE_URL = `http://localhost`
+
+const JOIN_API_URL = `${API_BASE_URL}/v1/join`
+const LOGIN_API_URL = `${API_BASE_URL}/v1/login`
+
+const LAST_BOOK_API_URL = `${API_BASE_URL}/v1/book/last`
+const READ_TIME_API_URL = `${API_BASE_URL}/v1/statistics/read-time/14`
+const STATISTICS_SUMMARY_URL = `${API_BASE_URL}/v1/statistics/year/2022`
+
+const ADD_BOOK_API_URL = `${API_BASE_URL}/v1/book`
+
+const READING_SESSION_CURRENT_API_URL = `${API_BASE_URL}/v1/reading-session/current`
+
+export {
+	API_BASE_URL,
+	JOIN_API_URL,
+	LOGIN_API_URL,
+	LAST_BOOK_API_URL,
+	READ_TIME_API_URL,
+	STATISTICS_SUMMARY_URL,
+	ADD_BOOK_API_URL,
+	READING_SESSION_CURRENT_API_URL,
+}
