9c191f6f7be8fd79a15bb8f3e9182450d689b846
jinkyumpark <jinpark1025@gmail.com>
2022-12-01 21:53:33 +0900

    add qna repository

---
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/app/qna/QnaRepository.java
@@ -0,0 +1,6 @@
+package com.jinkyumpark.bookitout.app.qna;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface QnaRepository extends PagingAndSortingRepository<Qna, Long> {
+}
