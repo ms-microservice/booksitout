e914a7f351fb906f4f6afe4279b4d2bc6bd4206c
jinkyumpark <jinpark1025@gmail.com>
2022-12-01 21:52:05 +0900

    add qna model

---
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/app/qna/Qna.java
@@ -0,0 +1,30 @@
+package com.jinkyumpark.bookitout.app.qna;
+
+import com.jinkyumpark.bookitout.app.user.AppUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter @Setter
+@Entity(name = "Qna")
+@Table(name = "qna")
+public class Qna {
+    @SequenceGenerator(name = "qna_seq", sequenceName = "qna_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "qna_seq")
+    @Column(name = "qna_id", updatable = false)
+    @Id
+    private Long qnaId;
+
+    @Column(name = "question", nullable = false)
+    private String question;
+
+    @Column(name = "answer")
+    private String answer;
+
+    @ManyToOne
+    @JoinColumn(name = "app_user_id", referencedColumnName = "app_user_id", foreignKey = @ForeignKey(name = "qna_app_user_fk"))
+    private AppUser appUser;
+}
