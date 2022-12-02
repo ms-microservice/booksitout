91c929dc700a72072d1ef9c13ac0b255582ad721
jinkyumpark <jinpark1025@gmail.com>
2022-12-02 15:52:45 +0900

    BookRepository.java : add find all giveup books methods

--- BookItOut/src/main/java/com/jinkyumpark/bookitout/app/book/BookRepository.java
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/app/book/BookRepository.java
@@ -10,12 +10,16 @@ import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
     @Query("select b from Book b where b.appUser.appUserId = ?1")
     List<Book> findAllBooksByAppUserId(Long appUserId, Pageable pageRequest);
 
-    @Query("select b from Book b where b.appUser.appUserId = ?1 and b.currentPage = b.endPage")
+    @Query("select b from Book b where b.appUser.appUserId = :appUserId and b.currentPage = b.endPage")
     List<Book> findAllDoneBooks(Long appUserId, Pageable pageRequest);
 
-    @Query("select b from Book b where b.appUser.appUserId = ?1 and not b.currentPage = b.endPage")
+    @Query("select b from Book b where b.appUser.appUserId = :appUserId and b.isGiveUp = true")
+    List<Book> findAllGiveUpBooks(Long appUserId, Pageable pageRequest);
+
+    @Query("select b from Book b where b.appUser.appUserId = :appUserId and not b.currentPage = b.endPage")
     List<Book> findAllNotDoneBooks(Long appUserId, Pageable pageRequest);
 }
