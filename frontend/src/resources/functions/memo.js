01b60a6f244fe1da9d0dbe430f63c56edf1ca252
jinkyumpark <jinpark1025@gmail.com>
2022-12-05 17:34:38 +0900

    front : add getMemo function

--- frontend/src/resources/functions/memo.js
+++ frontend/src/resources/functions/memo.js
@@ -1,1 +1,12 @@
-const MEMO_API_URL = `http://localhost/v1/memo/`
+const MEMO_API_URL = `http://localhost/v1/memo/all/`
+
+const getMemo = (bookId, token, setMemoList) => {
+	fetch(MEMO_API_URL + bookId, {
+		method: 'GET',
+		headers: { Authorization: token },
+	})
+		.then((res) => res.json())
+		.then((memoList) => setMemoList(memoList))
+}
+
+export { getMemo }
