62c278344076261d4fd6686f58134f8ef213baf9
jinkyumpark <jinpark1025@gmail.com>
2022-12-05 17:34:50 +0900

    front : add getQuotation function

--- frontend/src/resources/functions/quotation.js
+++ frontend/src/resources/functions/quotation.js
@@ -1,1 +1,12 @@
-const QUOTE_API_URL = `http://localhost/v1/quote/`
+const QUOTE_API_URL = `http://localhost/v1/quotation/all/`
+
+const getQuotation = (bookId, token, setQuotationList) => {
+	fetch(QUOTE_API_URL + bookId, {
+		method: 'GET',
+		headers: { Authorization: token },
+	})
+		.then((res) => res.json())
+		.then((quotationList) => setQuotationList(quotationList))
+}
+
+export { getQuotation }
