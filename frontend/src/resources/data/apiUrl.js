1c914e98caf48705b79a988c987dd49986d13748
jinkyumpark <jinpark1025@gmail.com>
2022-12-11 05:16:48 +0900

    front : apiUrl : remove READ_TIME_API_URL

--- frontend/src/resources/data/apiUrl.js
+++ frontend/src/resources/data/apiUrl.js
@@ -4,7 +4,6 @@ const LOGIN_API_URL = `${API_BASE_URL}/login`
 const JOIN_API_URL = `${API_BASE_URL}/v1/join`
 
 const LAST_BOOK_API_URL = `${API_BASE_URL}/v1/book/last`
-const READ_TIME_API_URL = `${API_BASE_URL}/v1/statistics/read-time/13`
 const STATISTICS_SUMMARY_URL = `${API_BASE_URL}/v1/statistics/year/2022`
 
 const ADD_BOOK_API_URL = `${API_BASE_URL}/v1/book`
@@ -18,7 +17,6 @@ export {
 	JOIN_API_URL,
 	LOGIN_API_URL,
 	LAST_BOOK_API_URL,
-	READ_TIME_API_URL,
 	STATISTICS_SUMMARY_URL,
 	ADD_BOOK_API_URL,
 	READING_SESSION_CURRENT_API_URL,
