5d528a379f8800849732b9edd639c7c6841394c7
jinkyumpark <jinpark1025@gmail.com>
2022-12-22 11:58:24 +0900

    front : add/edit/delete/get goal

--- frontend/src/components/statistics/Goal.js
+++ frontend/src/components/statistics/Goal.js
@@ -2,29 +2,62 @@ import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { Card, Button } from 'react-bootstrap'
 // Components
-import GoalView from './GoalView'
 import NoContent from '../common/NoContent'
+import GoalView from './GoalView'
+import GoalAddModal from './GoalAddModal'
+
 // Functions
+import { deleteGoal, getGoalList } from '../../functions/goal'
+import GoalEditModal from './GoalEditModal'
 
 const Goal = () => {
-	const [goalList, setGoalList] = useState([{ year: 2022, current: 0, goal: 50 }])
+	const currentYear = new Date().getFullYear()
+
+	const [goalList, setGoalList] = useState([])
 	const [currentYearGoal, setCurrentYearGoal] = useState(null)
 	const [highlightBookList, setHighlightBookList] = useState([])
 
-	const currentYear = new Date().getFullYear()
+	const handleDeleteGoal = () => {
+		const confirm = window.confirm('목표를 삭제할까요?')
+
+		if (confirm) {
+			deleteGoal(currentYear).then((success) => {
+				if (success) {
+					setCurrentYearGoal(null)
+				}
+			})
+		}
+	}
 
 	useEffect(() => {
 		// TODO fetch goalList
+		getGoalList().then((goalListData) => {
+			setGoalList(goalListData)
 
-		const currentYearGoalOptional = goalList.find((goal) => goal.year === currentYear)
-		if (typeof currentYearGoalOptional != 'undefined') {
-			setCurrentYearGoal(currentYearGoalOptional)
-		}
+			const currentYearGoalOptional = goalListData.find((goal) => goal.year == currentYear)
+			if (typeof currentYearGoalOptional != 'undefined') {
+				setCurrentYearGoal(currentYearGoalOptional)
+			}
+		})
 	}, [])
+
+	const [isGoalModalOpen, setIsGoalModalOpen] = useState(false)
+	const [isGoalEditModalOpen, setIsGoalEditModalOpen] = useState(false)
 
 	return (
 		<div className='container'>
 			<div className='row row-eq-height'>
+				<GoalAddModal isModalOpen={isGoalModalOpen} setIsModalOpen={setIsGoalModalOpen} setCurrentYearGoal={setCurrentYearGoal} />
+
+				{currentYearGoal != null && (
+					<GoalEditModal
+						isModalOpen={isGoalEditModalOpen}
+						setIsModalOpen={setIsGoalEditModalOpen}
+						setCurrentYearGoal={setCurrentYearGoal}
+						currentBook={currentYearGoal.current}
+					/>
+				)}
+
 				<div className='col-12 col-lg-6 mb-4'>
 					<Card className='h-100'>
 						<Card.Body>
@@ -32,7 +65,7 @@ const Goal = () => {
 
 							<div className={currentYearGoal != null && 'mb-5'}>
 								{currentYearGoal == null ? (
-									<NoContent message={`${currentYear}년 목표가 없어요 설정해 주세요!`} style={{ width: '150px' }} />
+									<NoContent message={`${currentYear}년 목표가 없어요`} style={{ width: '150px' }} />
 								) : (
 									<GoalView goal={currentYearGoal} />
 								)}
@@ -41,17 +74,19 @@ const Goal = () => {
 							<div className='row justify-content-center'>
 								{currentYearGoal == null ? (
 									<div className='col-12 col-lg-8'>
-										<Button className='w-100 mt-3'>목표 설정하기</Button>
+										<Button className='w-100 mt-3' onClick={() => setIsGoalModalOpen(true)}>
+											목표 설정하기
+										</Button>
 									</div>
 								) : (
 									<>
 										<div className='col-6'>
-											<Button variant='warning' className='w-100'>
+											<Button variant='warning' className='w-100' onClick={() => setIsGoalEditModalOpen(true)}>
 												목표 수정하기
 											</Button>
 										</div>
 										<div className='col-6'>
-											<Button variant='danger' className='w-100'>
+											<Button variant='danger' className='w-100' onClick={handleDeleteGoal}>
 												목표 삭제하기
 											</Button>
 										</div>
