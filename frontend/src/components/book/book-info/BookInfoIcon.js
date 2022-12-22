f5a5e8ab6162e50c7cd3ba23d98003ebd918e939
jinkyumpark <jinpark1025@gmail.com>
2022-12-23 08:14:25 +0900

    front : book info icon responsive support

--- frontend/src/components/book/book-info/BookInfoIcon.js
+++ frontend/src/components/book/book-info/BookInfoIcon.js
@@ -4,19 +4,19 @@ import { Card } from 'react-bootstrap'
 const BookInfoIcon = ({
 	infoType,
 	infoData,
-	responsiveImageStyle = 'col-4 col-md-8 align-self-center',
-	responsiveTextStyle = 'col-6 col-md-12 mt-2 mt-md-0',
+	responsiveImageStyle = 'col-9 col-md-8 align-self-center',
+	responsiveTextStyle = 'col-12 col-md-12 mt-2 mt-md-0',
 }) => {
 	return (
 		<Card>
 			<Card.Body>
 				<div className='row justify-content-center'>
 					<div className={responsiveImageStyle}>
-						<img src={infoType.imageFunction(infoData)} alt='' className='img-fluid mb-2' />
+						<img src={infoType.imageFunction(infoData)} alt='' className='img-fluid mb-0 mb-md-2' />
 					</div>
 
 					<div className={responsiveTextStyle}>
-						<h5 className='text-center'>{infoType.textFunction(infoData)}</h5>
+						<h5 className='text-center mb-0'>{infoType.textFunction(infoData)}</h5>
 					</div>
 				</div>
 			</Card.Body>
