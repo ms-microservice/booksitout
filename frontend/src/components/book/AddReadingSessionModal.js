bea2d2a13c5f79b8afa525ebb24da69cba61319b
jinkyumpark <jinpark1025@gmail.com>
2022-12-24 03:29:46 +0900

    front : add AddReadingSessionModal.js

---
+++ frontend/src/components/book/AddReadingSessionModal.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
+import toast from 'react-hot-toast'
+import { addReadingSession } from '../../functions/reading'
+import '../../resources/css/input.css'
+
+const AddReadingSessionModal = ({ isModalOpen, setIsModalOpen, book, readingSessionList, setReadingSessionList }) => {
+	const [startPage, setStartPage] = useState(null)
+	const [endPage, setEndPage] = useState(null)
+	const [readTime, setReadTime] = useState(null)
+	const [year, setYear] = useState(new Date().getFullYear())
+	const [month, setMonth] = useState(new Date().getMonth() + (1 % 12))
+	const [day, setDay] = useState(new Date().getDate())
+
+	const handleAddReadingSession = (e) => {
+		e.preventDefault()
+
+		if (startPage == null) {
+			toast.error('시작 페이지를 입력해 주세요')
+			return
+		}
+
+		if (endPage == null) {
+			toast.error('끝 페이지를 입력해 주세요')
+			return
+		}
+
+		if (readTime == null) {
+			toast.error('독서 활동 시간을 입력해 주세요')
+			return
+		}
+
+		addReadingSession(book.bookId, year, month, day, startPage, endPage, readTime).then((success) => {
+			if (success) {
+				setReadingSessionList([
+					...readingSessionList,
+					{
+						startPage: startPage,
+						endPage: endPage,
+						startTime: `${year}-${month}-${day}T`,
+						endTime: `${year}-${month}-${day}T`,
+						readTime: readTime,
+					},
+				])
+				setIsModalOpen(false)
+				toast.success('독서활동을 직접 추가했어요')
+			} else {
+				toast.error('오류가 났어요. 잠시 후 다시 시도해 주세요')
+			}
+		})
+	}
+
+	const getDayCountOfMonth = (year, month) => {
+		return new Date(year, month, 0).getDate(book.bookId, year, month, day, startPage, endPage, readTime)
+	}
+
+	return (
+		<Modal show={isModalOpen} onHide={() => setIsModalOpen(false)} fullscreen='md-down'>
+			<Modal.Header closeButton className='text-center'>
+				<h4 className='w-100'>독서 활동 직접 추가하기</h4>
+			</Modal.Header>
+
+			<Modal.Body>
+				<Form onSubmit={(e) => handleAddReadingSession(e)}>
+					<Form.Label>🗓️ 날짜</Form.Label>
+					<div className='row'>
+						<div className='col-4'>
+							<Form.Select className='mb-2' value={year} onChange={(e) => setYear(e.target.value)}>
+								{Array.from({ length: 5 }, (_, i) => i + (new Date().getFullYear() - 5 + 1))
+									.reverse()
+									.map((yearValue) => {
+										return <option value={yearValue}>{yearValue.toString().substring(2)}년</option>
+									})}
+							</Form.Select>
+						</div>
+
+						<div className='col-4'>
+							<Form.Select className='mb-2' value={month} onChange={(e) => setMonth(e.target.value)}>
+								{Array.from({ length: 12 }, (_, i) => i + 1).map((monthValue) => {
+									return <option value={monthValue}>{monthValue}월</option>
+								})}
+							</Form.Select>
+						</div>
+
+						<div className='col-4'>
+							<Form.Select className='mb-2' value={day} onChange={(e) => setDay(e.target.value)}>
+								{Array.from({ length: getDayCountOfMonth(year, month) }, (_, i) => i + 1).map((dayValue) => {
+									return <option value={dayValue}>{dayValue}일</option>
+								})}
+							</Form.Select>
+						</div>
+					</div>
+
+					<Form.Label>시작 페이지</Form.Label>
+					<Form.Control className='mb-2' type='number' onChange={(e) => setStartPage(e.target.value)} />
+
+					<Form.Label>끝 페이지</Form.Label>
+					<Form.Control className='mb-2' type='number' onChange={(e) => setEndPage(e.target.value)} />
+
+					<Form.Label>⏰ 시간 (분)</Form.Label>
+					<Form.Control className='mb-2' type='number' onChange={(e) => setReadTime(e.target.value)} />
+
+					<Button variant='success' type='submit' className='w-100 mt-2'>
+						추가하기
+					</Button>
+				</Form>
+			</Modal.Body>
+		</Modal>
+	)
+}
+
+export default AddReadingSessionModal
