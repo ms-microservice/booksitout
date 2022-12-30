0ef06c7e6ebea5dfef509c25c25203a96bc58425
jinkyumpark <jinpark1025@gmail.com>
2022-12-30 09:10:37 +0900

    front : change date function to ts

---
+++ frontend/src/functions/date.ts
@@ -0,0 +1,11 @@
+const getDateDifferenceInDays = (date1: Date, date2: Date): number => {
+	const differenceInTime = Math.abs(date1.getTime() - date2.getTime())
+	const differenceInDays = differenceInTime / (1000 * 60 * 60 * 24)
+	return differenceInDays
+}
+
+const getDayCountOfMonth = (year: number, month: number): number => {
+	return new Date(year, month, 0).getDate()
+}
+
+export { getDateDifferenceInDays, getDayCountOfMonth }
