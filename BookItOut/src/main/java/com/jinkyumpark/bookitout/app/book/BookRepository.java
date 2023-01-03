e9f3d78b5d68b766140bd1c451d271f9a6f8e78e
jinkyumpark <jinpark1025@gmail.com>
2023-01-03 10:56:13 +0900

    fix bug : book list desc

--- BookItOut/src/main/java/com/jinkyumpark/bookitout/app/book/BookRepository.java
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/app/book/BookRepository.java
@@ -14,18 +14,18 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Query("select b from Book b where b.appUser.appUserId = ?1 order by b.lastModified")
     List<Book> findAllBooks(Long appUserId, Pageable pageRequest);
 
-    @Query("select b from Book b where b.appUser.appUserId = ?1 and b.currentPage = 0 and b.isGiveUp = false order by b.lastModified")
+    @Query("select b from Book b where b.appUser.appUserId = ?1 and b.currentPage = 0 and b.isGiveUp = false order by b.lastModified desc")
     List<Book> findAllNotStartedBooks(Long appUserId, Pageable pageRequest);
 
-    @Query("select b from Book b where b.appUser.appUserId = ?1 and b.currentPage > 0 and b.currentPage < b.endPage and b.isGiveUp = false order by b.lastModified")
+    @Query("select b from Book b where b.appUser.appUserId = ?1 and b.currentPage > 0 and b.currentPage < b.endPage and b.isGiveUp = false order by b.lastModified desc")
     List<Book> findAllStartedBooks(Long appUserId, Pageable pageRequest);
 
-    @Query("select b from Book b where b.appUser.appUserId = :appUserId and b.currentPage = b.endPage order by b.lastModified")
+    @Query("select b from Book b where b.appUser.appUserId = :appUserId and b.currentPage = b.endPage order by b.lastModified desc")
     List<Book> findAllDoneBooks(Long appUserId, Pageable pageRequest);
 
-    @Query("select b from Book b where b.appUser.appUserId = :appUserId and not b.currentPage = b.endPage and b.isGiveUp = false order by b.lastModified")
+    @Query("select b from Book b where b.appUser.appUserId = :appUserId and not b.currentPage = b.endPage and b.isGiveUp = false order by b.lastModified desc")
     List<Book> findAllNotDoneBooks(Long appUserId, Pageable pageRequest);
 
-    @Query("select b from Book b where b.appUser.appUserId = :appUserId and b.isGiveUp = true and b.currentPage < b.endPage order by b.lastModified")
+    @Query("select b from Book b where b.appUser.appUserId = :appUserId and b.isGiveUp = true and b.currentPage < b.endPage order by b.lastModified desc")
     List<Book> findAllGiveUpBooks(Long appUserId, Pageable pageRequest);
 }
