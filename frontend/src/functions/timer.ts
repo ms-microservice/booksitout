94429c3bcccbdd00a3e81d18742e0d09d4e744a5
jinkyumpark <jinpark1025@gmail.com>
2023-01-14 01:12:24 +0900

    front : refactor timer to ts

--- frontend/src/functions/timer.ts
+++ frontend/src/functions/timer.ts
@@ -7,36 +7,36 @@ const getIsTimerOn = () => {
 }
 
 const turnOnTimer = () => {
-	localStorage.setItem(TIMER_ON_KEY, true)
+	localStorage.setItem(TIMER_ON_KEY, 'true')
 }
 
 const turnOffTimer = () => {
-	localStorage.setItem(TIMER_ON_KEY, false)
+	localStorage.setItem(TIMER_ON_KEY, 'false')
 }
 
-const getDifferenceInSeconds = (date1, date2) => {
+const getDifferenceInSeconds = (date1: Date, date2: Date) => {
 	return Math.abs((Number(date1.getTime()) - Number(date2.getTime())) / 1000)
 }
 
 const getTimerSecond = () => {
-	const difference = getDifferenceInSeconds(new Date(localStorage.getItem(READING_TIME_DATE)), new Date())
-	localStorage.setItem(READING_TIME_DATE, new Date())
+	const difference = getDifferenceInSeconds(new Date(localStorage.getItem(READING_TIME_DATE) ?? ''), new Date())
+	localStorage.setItem(READING_TIME_DATE, new Date().toString())
 
 	if (difference > 3 && getIsTimerOn()) {
 		const previousSeconds = localStorage.getItem(READING_TIME_KEY)
-		localStorage.setItem(READING_TIME_KEY, Number(previousSeconds) + Number(difference))
+		localStorage.setItem(READING_TIME_KEY, (Number(previousSeconds) + Number(difference)).toString())
 	}
 
 	return localStorage.getItem(READING_TIME_KEY)
 }
 
 const updateReadingTimeDate = () => {
-	localStorage.setItem(READING_TIME_DATE, new Date())
+	localStorage.setItem(READING_TIME_DATE, new Date().toString())
 }
 
-const updateTimerSecond = (newTimerSecond) => {
-	localStorage.setItem(READING_TIME_DATE, new Date())
-	localStorage.setItem(READING_TIME_KEY, newTimerSecond)
+const updateTimerSecond = (newTimerSecond: number) => {
+	localStorage.setItem(READING_TIME_DATE, new Date().toString())
+	localStorage.setItem(READING_TIME_KEY, newTimerSecond.toString())
 }
 
 export { getIsTimerOn, getTimerSecond, updateTimerSecond, turnOnTimer, turnOffTimer, updateReadingTimeDate }
