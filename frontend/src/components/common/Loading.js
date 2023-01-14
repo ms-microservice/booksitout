8babc20a23c1ddba6e3899ec18dc761c4187ac53
jinkyumpark <jinpark1025@gmail.com>
2023-01-14 15:53:54 +0900

    refactor Loading.js

--- frontend/src/components/common/Loading.js
+++ frontend/src/components/common/Loading.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Loading = () => {
+const Loading = ({ message = '잠시만 기다려 주세요', textSize = 'h1' }) => {
 	const spinnerStyle = {
 		width: '100px',
 		height: '100px',
@@ -11,7 +11,7 @@ const Loading = () => {
 			<div className='row justify-content-center text-center align-items-center'>
 				<div className='col-12 col-md-6'>
 					<div className='spinner-border text-success' role='status' style={spinnerStyle} />
-					<h1 className='mt-4'>잠시만 기다려 주세요</h1>
+					<div className={`mt-4 ${textSize}`}>{message}</div>
 				</div>
 			</div>
 		</div>
