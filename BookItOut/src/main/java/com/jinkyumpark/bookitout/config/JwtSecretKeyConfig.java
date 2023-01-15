5ab58f09685e1566d6625d273f8642b23215eb60
jinkyumpark <jinpark1025@gmail.com>
2023-01-15 15:06:03 +0900

    cleanup

--- BookItOut/src/main/java/com/jinkyumpark/bookitout/config/JwtSecretKeyConfig.java
+++ BookItOut/src/main/java/com/jinkyumpark/bookitout/config/JwtSecretKeyConfig.java
@@ -1,14 +1,13 @@
 package com.jinkyumpark.bookitout.config;
 
-import com.jinkyumpark.bookitout.config.JwtConfig;
 import io.jsonwebtoken.security.Keys;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.crypto.SecretKey;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Configuration
 public class JwtSecretKeyConfig {
     private final JwtConfig jwtConfig;
