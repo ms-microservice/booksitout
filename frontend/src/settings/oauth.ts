7ac5fef9b34d70c9ef34d96e9e39a43efd76d585
jinkyumpark <jinpark1025@gmail.com>
2023-01-20 21:42:17 +0900

    front : fix bugs

--- frontend/src/settings/oauth.ts
+++ frontend/src/settings/oauth.ts
@@ -1,10 +1,16 @@
+const localBase = process.env.REACT_APP_LOCAL_URL
+
 const kakao = {
 	clientId: 'e0b8e02a9826e15029e2182d1d03bf2b',
 	responseType: 'code',
+	redirectUrl: `${localBase}/login/oauth/kakao/`,
 }
 
 const naver = {
 	clientId: 'WWI0nkWyzfAIMmjR0Y8N',
+	responseType: 'code',
+	redirectUrl: `${localBase}/login/oauth/naver/`,
+	state: 'bookitout',
 }
 
 const google = {}
