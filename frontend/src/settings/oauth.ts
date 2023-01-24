661833393c779cce71657be635e0c938c1b17b5e
jinkyumpark <jinpark1025@gmail.com>
2023-01-24 21:06:18 +0900

    front : oauth : add facebook settings

--- frontend/src/settings/oauth.ts
+++ frontend/src/settings/oauth.ts
@@ -14,10 +14,14 @@ const naver = {
 const google = {
 	clientId: '1054186388784-pvv99ifu26lc883obgl6lf05s0utdv7v.apps.googleusercontent.com',
 	responseType: 'code',
-	redirectUrl: `https%3A%2F%2Fbook.jinkyumpark.com%2Flogin%2Foauth%2Fgoogle`,
+	redirectUrl: `https%3A%2F%2Fbook.jinkyumpark.com%2Flogin%2Foauth%2Fgoogle%2F`,
 	scope: `https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile`,
 }
 
-const facebook = {}
+const facebook = {
+	clientId: ``,
+	redirectUrl: `https%3A%2F%2Fbook.jinkyumpark.com%2Flogin%2Foauth%2Ffacebook`,
+	state: ``,
+}
 
 export { kakao, naver, google, facebook }
