483e8176c6eb9da21a695f9476e13dc0c9566295
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 21:58:14 +0900

    front : redux : add modalSlice

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 import timerReducer from './timerSlice'
+import modalReducer from './modalSlice'
 
-export default configureStore({
+const store = configureStore({
 	reducer: {
 		user: userReducer,
 		timer: timerReducer,
+		modal: modalReducer,
 	},
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export default store
