f5f7585cc3992b6c3ef99a180a1d8e2d460f00c4
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 19:30:26 +0900

    front : add search functions

--- frontend/src/functions/search.ts
+++ frontend/src/functions/search.ts
@@ -1,13 +1,46 @@
-import toast from 'react-hot-toast'
+import axios from 'axios'
+import urls from '../settings/urls'
+import utils from './utils'
 
-const search = (keyword, e, navigate) => {
-	e.preventDefault()
+const search = {
+	myBook: (query: string) => {
+		return axios
+			.get(urls.api.search.myBook(query), { headers: { Authorization: utils.getToken() } })
+			.then((res) => res.data)
+			.catch(() => {
+				return []
+			})
+	},
 
-	if (keyword !== '') {
-		navigate(`/search/${keyword}`)
-	} else {
-		toast.error('검색어를 입력해 주세요')
-	}
+	used: (query: string, include: string) => {
+		return axios
+			.get(urls.api.search.used(query, include))
+			.then((res) => res.data)
+			.catch(() => {
+				return []
+			})
+	},
+
+	library: (query: string, region: string, regionDetail: string) => {
+		return axios
+			.get(urls.api.search.libraryByRegion(query, region, regionDetail))
+			.then((res) => res.data)
+			.catch(() => {
+				return []
+			})
+	},
+
+	splitArray: (originalArray: any[], count: number) => {
+		if (originalArray == null) return [[]]
+
+		const splitList: any[][] = []
+
+		for (let i = 0; i < originalArray.length; i += 2) {
+			splitList.push([originalArray[i], originalArray.length - 1 === i ? null : originalArray[i + 1]])
+		}
+
+		return splitList
+	},
 }
 
-export { search }
+export default search
