64a8a30764ce61c7715b49dbf0b1969bdb998229
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 19:31:36 +0900

    add Facebook OAuth Dtos

---
+++ src/main/java/com/jinkyumpark/bookitout/user/oauth/facebook/FacebookUserInfo.java
@@ -0,0 +1,11 @@
+package com.jinkyumpark.bookitout.user.oauth.facebook;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Builder
+@Getter
+public class FacebookUserInfo {
+}
