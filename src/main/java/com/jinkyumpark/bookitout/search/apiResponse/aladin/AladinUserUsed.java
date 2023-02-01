74adc6a69b316b51fc2615af78a900c681fedec5
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 19:21:57 +0900

    add Aladin API Response Dtos

---
+++ src/main/java/com/jinkyumpark/bookitout/search/apiResponse/aladin/AladinUserUsed.java
@@ -0,0 +1,13 @@
+package com.jinkyumpark.bookitout.search.apiResponse.aladin;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+public class AladinUserUsed {
+    private Integer itemCount;
+    private Integer minPrice;
+    private String link;
+}
