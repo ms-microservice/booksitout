70109324fab66a54caa71793863465882fdc9aa3
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 19:32:34 +0900

    add UsedBookProvider.java

---
+++ src/main/java/com/jinkyumpark/bookitout/search/response/used/UsedBookProvider.java
@@ -0,0 +1,10 @@
+package com.jinkyumpark.bookitout.search.response.used;
+
+public enum UsedBookProvider {
+    ONLINE_ALADIN,
+    ONLINE_YES24,
+    ONLINE_KYOBO,
+
+    OFFLINE_ALADIN,
+    OFFLINE_YES24
+}
