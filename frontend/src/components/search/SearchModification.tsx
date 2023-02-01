bf99409dfa35c98b95a5b76d20042dfc9dd86e74
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 19:25:44 +0900

    front : add SearchModification.tsx

---
+++ frontend/src/components/search/SearchModification.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const SearchModification = () => {
+	return (
+		<Card>
+			<Card.Body>
+				<h5>검색조건</h5>
+			</Card.Body>
+		</Card>
+	)
+}
+
+export default SearchModification
