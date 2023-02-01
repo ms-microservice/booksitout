e0440bf64d1470aa2b7e080722b752748edea6c1
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 21:57:22 +0900

    front : refactor error

--- frontend/src/components/common/Error.js
+++ frontend/src/components/common/Error.js
@@ -1,6 +1,6 @@
 import { BiErrorCircle as ErrorIcon } from 'react-icons/bi'
 
-const Error = ({ message = '오류가 났어요' }) => {
+const Error = ({ message = '오류가 났어요', color = 'text-danger' }) => {
 	const iconStyle = {
 		width: '100px',
 		height: '100px',
@@ -9,7 +9,7 @@ const Error = ({ message = '오류가 났어요' }) => {
 	return (
 		<div className='row justify-content-center mt-5 mb-5'>
 			<div className='col-8 text-center'>
-				<ErrorIcon className='text-danger' style={iconStyle} />
+				<ErrorIcon className={color} style={iconStyle} />
 
 				<h3 className='mt-3'>{message}</h3>
 			</div>
