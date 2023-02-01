1753033e60c5dd5c641e3478ce23ebc9fd9cd93c
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 19:33:57 +0900

    add Korea region enum

---
+++ src/main/java/com/jinkyumpark/bookitout/search/request/KoreanRegion.java
@@ -0,0 +1,32 @@
+package com.jinkyumpark.bookitout.search.request;
+
+import lombok.Getter;
+
+@Getter
+public enum KoreanRegion {
+    SEOUL("서울", 11),
+    BUSAN("부산", 21),
+    DAEGU("대구", 22),
+    INCHEON("인천", 23),
+    GWANGJU("광주", 24),
+    DAEJEON("대전", 25),
+    ULSAN("울산", 26),
+    SEJONG("세종", 29),
+    GYEONGGIDO("경기도", 31),
+    GANGWONDO("강원도", 32),
+    CHUNGCHEONGBUKDO("충청북도", 33),
+    CHUNGCHEONGNAMDO("충청남도", 34),
+    JEOLLABUKDO("전라북도", 35),
+    JEOLLANAMDO("전라남도", 36),
+    GYEONGSANGBUKDO("경상북도", 37),
+    GYEONGSANGNAMDO("경상남도", 38),
+    JEJU("제주", 39);
+
+    private final String koreanName;
+    private final Integer apiRegionCode;
+
+    KoreanRegion(String koreanName, Integer regionCode) {
+        this.koreanName = koreanName;
+        this.apiRegionCode = regionCode;
+    }
+}
