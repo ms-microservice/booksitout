a53b0a337a661b8988a16173c92415a1af99cfb7
jinkyumpark <jinpark1025@gmail.com>
2023-02-01 19:31:56 +0900

    add HasBook Dtos

---
+++ src/main/java/com/jinkyumpark/bookitout/search/apiResponse/library/offline/hasbook/HasBookResponse.java
@@ -0,0 +1,11 @@
+package com.jinkyumpark.bookitout.search.apiResponse.library.offline.hasbook;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+public class HasBookResponse {
+    private HasBookResponseResponse response;
+}
---
+++ src/main/java/com/jinkyumpark/bookitout/search/apiResponse/library/offline/hasbook/HasBookResult.java
@@ -0,0 +1,12 @@
+package com.jinkyumpark.bookitout.search.apiResponse.library.offline.hasbook;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+public class HasBookResult {
+    private String hasBook;
+    private String loanAvailable;
+}
