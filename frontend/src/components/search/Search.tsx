1a8e3d87f9333a3a0b810d1cce822ab129b01f29
jinkyumpark <jinpark1025@gmail.com>
2023-02-02 18:11:13 +0900

    front : add Search settings

--- frontend/src/components/search/Search.tsx
+++ frontend/src/components/search/Search.tsx
@@ -3,15 +3,22 @@ import { useParams } from 'react-router-dom'
 // Components
 import Loading from '../common/Loading'
 import BookSearchResult from './BookSearchResult'
-import UsedBookCardComponent from './cardComponent/UsedBookCardComponent'
-import MyBookCardComponent from './cardComponent/MyBookCardComponent'
-import LibraryCardComponent from './cardComponent/LibraryCardComponent'
+
+import MyBookComponent from './cardComponent/MyBookCardComponent'
+import UsedBookComponent from './cardComponent/UsedBookCardComponent'
+import UsedOnlineLabel from './label/UsedOnlineLabel'
+import UsedOfflineLabel from './label/UsedOfflineLabel'
+import SubscriptionComponent from './cardComponent/SubscriptionCardComponent'
+import SubscriptionLabel from './label/SubscriptionLabel'
+import LibraryComponent from './cardComponent/LibraryCardComponent'
+import LibraryLabel from './label/LibraryLabel'
+import LibraryOnlineLabel from './label/OnlineLibraryLabel'
+import LibraryDetailModal from './LibraryDetailModal'
 // Functions
 import search from '../../functions/search'
 import uiSettings from '../../settings/ui'
 // Types
-import { UsedBook, MyBook, LibraryBook } from './BookType'
-import LibraryDetailModal from './LibraryDetailModal'
+import { UsedBook, MyBook, LibraryBook, SubscriptionBook } from './BookType'
 
 const Search = () => {
 	const { query } = useParams()
@@ -25,28 +32,25 @@ const Search = () => {
 	}, [])
 
 	const [myBookList, setMyBookList] = useState<MyBook[]>([])
-	const [libraryBookList, setLibraryBookList] = useState<LibraryBook[]>([])
-	const [onlineLibraryBookList, setOnlineLibraryBookList] = useState([])
-	const [onlineUsedBookList, setOnlineUsedBookList] = useState<UsedBook[]>([])
-	const [offlineUsedBookList, setOfflineUsedBookList] = useState<UsedBook[]>([])
-	const [subscriptionBookList, setSubscriptionBookList] = useState([])
+	const [libraryList, setLibraryBookList] = useState<LibraryBook[]>([])
+	const [onlineLibraryList, setOnlineLibraryBookList] = useState([])
+	const [onlineUsedList, setOnlineUsedBookList] = useState<UsedBook[]>([])
+	const [offlineUsedList, setOfflineUsedBookList] = useState<UsedBook[]>([])
+	const [subscriptionList, setSubscriptionBookList] = useState<SubscriptionBook[]>([])
 	useEffect(() => {
 		setLoading(true)
 
-		setMyBookList([])
-		setLibraryBookList([])
-		setOnlineLibraryBookList([])
-		setOnlineUsedBookList([])
-		setOfflineUsedBookList([])
-		setSubscriptionBookList([])
-
 		Promise.all([
 			search.myBook(query || '').then((result) => setMyBookList(result)),
-			search.used(query || '', 'ALADIN').then((result) => {
-				setOnlineUsedBookList(result.online)
-				setOfflineUsedBookList(result.offline)
-			}),
-			search.library(query || '', 'SEOUL', 'YEONGDEUNGPOGU').then((result) => setLibraryBookList(result)),
+			search.settings.library.isConfigured() && search.library(query || '', search.settings.library.api.region(), search.settings.library.api.regionDetail()).then((result) => setLibraryBookList(result)),
+
+			(search.settings.usedOnline.isConfigured() || search.settings.usedOffline.isConfigured()) &&
+				search.used(query || '', 'ALADIN,YES24').then((result) => {
+					setOnlineUsedBookList(result.online)
+					setOfflineUsedBookList(result.offline)
+				}),
+
+			search.settings.subscription.isConfigured() && search.subscription(query || '', search.settings.subscription.api()).then((result) => setSubscriptionBookList(result)),
 		]).finally(() => {
 			setLoading(false)
 			setInitial(false)
@@ -59,14 +63,12 @@ const Search = () => {
 		<div className='container mt-5'>
 			<LibraryDetailModal />
 
-			<BookSearchResult label='내 책' bookList={myBookList} CardComponent={MyBookCardComponent} />
-			<BookSearchResult label='도서관' bookList={libraryBookList} CardComponent={LibraryCardComponent} />
-
-			<BookSearchResult label='전자도서관' bookList={onlineLibraryBookList} CardComponent={<></>} />
-			<BookSearchResult label='구독 플레폼' bookList={subscriptionBookList} CardComponent={<></>} />
-
-			<BookSearchResult label='중고책 (온라인)' bookList={onlineUsedBookList} CardComponent={UsedBookCardComponent} />
-			<BookSearchResult label='중고책 (매장)' bookList={offlineUsedBookList} CardComponent={UsedBookCardComponent} />
+			<BookSearchResult label='내 책' labelComponent={<></>} bookList={myBookList} CardComponent={MyBookComponent} isConfigured={true} />
+			<BookSearchResult label='도서관' labelComponent={<LibraryLabel />} bookList={libraryList} CardComponent={LibraryComponent} isConfigured={search.settings.library.isConfigured()} />
+			<BookSearchResult label='전자도서관' labelComponent={<LibraryOnlineLabel />} bookList={onlineLibraryList} CardComponent={<></>} isConfigured={search.settings.onlineLibrary.isConfigured()} />
+			<BookSearchResult label='구독' labelComponent={<SubscriptionLabel />} bookList={subscriptionList} CardComponent={SubscriptionComponent} isConfigured={search.settings.subscription.isConfigured()} />
+			<BookSearchResult label='중고책 (온라인)' labelComponent={<UsedOnlineLabel />} bookList={onlineUsedList} CardComponent={UsedBookComponent} isConfigured={search.settings.usedOnline.isConfigured()} />
+			<BookSearchResult label='중고책 (매장)' labelComponent={<UsedOfflineLabel />} bookList={offlineUsedList} CardComponent={UsedBookComponent} isConfigured={search.settings.usedOffline.isConfigured()} />
 		</div>
 	)
 }
--- frontend/src/functions/search.ts
+++ frontend/src/functions/search.ts
@@ -41,6 +41,79 @@ const search = {
 
 		return splitList
 	},
+
+	settings: {
+		library: {
+			isConfigured: (): boolean => {
+				return search.settings.library.api.region() !== '' && search.settings.library.api.regionDetail() !== ''
+			},
+
+			display: {
+				region: () => {
+					return localStorage.getItem('search-library-region-display') ?? ''
+				},
+				regionDetail: () => {
+					return localStorage.getItem('search-library-region-detail-display') ?? ''
+				},
+			},
+			api: {
+				region: (): string => {
+					return localStorage.getItem('search-library-region-api') ?? ''
+				},
+				regionDetail: (): string => {
+					return localStorage.getItem('search-library-region-detail-api') ?? ''
+				},
+			},
+		},
+
+		onlineLibrary: {
+			isConfigured: (): boolean => {
+				return search.settings.onlineLibrary.display() !== '' && search.settings.onlineLibrary.api() !== ''
+			},
+			display: () => {
+				return localStorage.getItem('search-online-library-display') ?? ''
+			},
+			api: () => {
+				return localStorage.getItem('search-online-library-api') ?? ''
+			},
+		},
+
+		subscription: {
+			isConfigured: (): boolean => {
+				return search.settings.subscription.display() !== '' && search.settings.subscription.api() !== ''
+			},
+			display: () => {
+				return localStorage.getItem('search-subscription-display') ?? ''
+			},
+			api: () => {
+				return localStorage.getItem('search-subscription-api') ?? ''
+			},
+		},
+
+		usedOnline: {
+			isConfigured: (): boolean => {
+				return search.settings.usedOnline.display() !== '' && search.settings.usedOnline.api() !== ''
+			},
+			display: () => {
+				return localStorage.getItem('search-used-online-display') ?? ''
+			},
+			api: () => {
+				return localStorage.getItem('search-used-online-api') ?? ''
+			},
+		},
+
+		usedOffline: {
+			isConfigured: (): boolean => {
+				return search.settings.usedOffline.display() !== '' && search.settings.usedOffline.api() !== ''
+			},
+			display: () => {
+				return localStorage.getItem('search-used-offline-display') ?? ''
+			},
+			api: () => {
+				return localStorage.getItem('search-used-offline-api') ?? ''
+			},
+		},
+	},
 }
 
 export default search
