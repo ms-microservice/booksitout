4a218f8e70bd99d05385dd7271a962d22f091b9a
jinkyumpark <jinpark1025@gmail.com>
2023-02-05 00:48:51 +0900

    front : add border to online library places

--- frontend/src/components/search/label/OnlineLibraryLabel.tsx
+++ frontend/src/components/search/label/OnlineLibraryLabel.tsx
@@ -13,12 +13,12 @@ const OnlineLibraryLabel = () => {
 
 	if (onlineLibraryList.length === 0 || typeof onlineLibraryList === 'undefined') return <></>
 	return (
-		<div className='row justify-content-end'>
+		<div className='row justify-content-end text-center'>
 			{onlineLibraryList.map((onlineLibrary) => {
 				return (
 					<a href={search.getLink(onlineLibrary?.key ?? '')} 
 						target='_blank' rel="noreferrer"
-						className='col-6 col-lg-2 mb-1 text-decoration-none text-black'>
+						className='col-6 col-lg-2 text-decoration-none text-black border'>
 						<span className='text-center text-secondary'>{onlineLibrary?.name}</span>
 					</a>
 				)
