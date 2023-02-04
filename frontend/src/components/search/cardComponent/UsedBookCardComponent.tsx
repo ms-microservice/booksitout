0f9a702a54f9d066eb555124d97953309783d0f9
jinkyumpark <jinpark1025@gmail.com>
2023-02-04 14:10:46 +0900

    front : UsedBookCardComponent.tsx : add money formatting

--- frontend/src/components/search/cardComponent/UsedBookCardComponent.tsx
+++ frontend/src/components/search/cardComponent/UsedBookCardComponent.tsx
@@ -1,6 +1,7 @@
 import { Card } from 'react-bootstrap'
 import { BsFillCartFill as CartIcon } from 'react-icons/bs'
 import uiSettings from '../../../settings/ui'
+import utils from '../../../functions/utils'
 
 import aladinIcon from '../../../resources/images/search/aladin.png'
 import yes24Icon from '../../../resources/images/search/yes24.png'
@@ -20,19 +21,8 @@ const UsedBookCardComponent = ({ book }) => {
 								<h5>{book.title.slice(0, 50)}</h5>
 								<h6 className='text-secondary'>{book.author}</h6>
 								<div className='mt-4'>
-									<img
-										src={
-											book.provider === 'ONLINE_ALADIN' || book.provider === 'OFFLINE_ALADIN'
-												? aladinIcon
-												: book.provider === 'ONLINE_YES24' || book.provider === 'OFFLINE_YES24'
-												? yes24Icon
-												: ''
-										}
-										alt=''
-										className='img-fluid rounded me-3 border'
-										style={{ width: '50px' }}
-									/>
-									재고 : {book.stockCount}개 ({book.minPrice}원)
+									<img src={book.provider === 'ONLINE_ALADIN' || book.provider === 'OFFLINE_ALADIN' ? aladinIcon : book.provider === 'ONLINE_YES24' || book.provider === 'OFFLINE_YES24' ? yes24Icon : ''} alt='' className='img-fluid rounded me-3 border' style={{ width: '50px' }} />
+									재고 : {book.stockCount}개 ({utils.formatMoney(book.minPrice)}원)
 									{book.locationList.length !== 0 && (
 										<div className='mt-2 text-secondary'>
 											{book.locationList.map((location) => {
