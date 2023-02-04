a89f9ccffc0340557793ea413b01849575c1124c
jinkyumpark <jinpark1025@gmail.com>
2023-02-04 14:18:51 +0900

    front : utils.ts : add money formatter

--- frontend/src/functions/utils.ts
+++ frontend/src/functions/utils.ts
@@ -1,11 +1,18 @@
+const moneyFormatter = new Intl.NumberFormat('kr-KO', {
+	style: 'currency',
+	currency: 'KRW',
+})
+
 const utils = {
 	getToken: () => localStorage.getItem('login-token'),
-	isEmailValid: (email) =>
-		email
-			.toLowerCase()
-			.match(
-				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-			),
+	isEmailValid: (email) => {
+		return email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+	},
+	formatMoney: (money: number | null) => {
+		if (money == null) return ''
+
+		return moneyFormatter.format(money)
+	},
 }
 
 export default utils
