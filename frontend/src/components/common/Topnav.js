de500c6ef1a7e9f981e049f6336a33ef701c9338
jinkyumpark <jinpark1025@gmail.com>
2023-02-06 17:02:42 +0900

    front : add search button in small screen

--- frontend/src/components/common/Topnav.js
+++ frontend/src/components/common/Topnav.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { Button, Container, Form, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Button, Container, Form, Navbar, Nav, NavDropdown, Card } from 'react-bootstrap'
 import toast from 'react-hot-toast'
 // React Icons
 import { HiOutlineUserAdd as JoinIcon } from 'react-icons/hi'
@@ -16,6 +16,11 @@ import { useSelector, useDispatch } from 'react-redux'
 import { logoutToken } from '../../redux/userSlice'
 import messages from '../../settings/messages'
 
+import {BiSearchAlt2 as SearchIcon} from 'react-icons/bi'
+
+import '../../resources/css/button.css'
+import '../../resources/css/topnav.css'
+
 const Topnav = () => {
 	const navigate = useNavigate()
 	const location = useLocation()
@@ -25,6 +30,8 @@ const Topnav = () => {
 	const expand = uiSettings.topnav.collapse
 
 	const [expanded, setExpanded] = useState(false)
+	const [showSearchBar, setShowSearchBar] = useState(false)
+	const [initialLoad, setInitialLoad] = useState(true)
 
 	const handleLogout = (e) => {
 		e.preventDefault()
@@ -42,126 +49,151 @@ const Topnav = () => {
 
 	useEffect(() => {
 		setExpanded(false)
+		setShowSearchBar(false)
 	}, [location.pathname])
 
 	return (
-		<Navbar key={expand} expand={expand} fixed='top' bg='light' collapseOnSelect expanded={expanded}>
-			<Container fluid>
-				<Navbar.Brand href={token === '' ? '/login' : '/'}>
-					<img src={logo} alt='' className='image-fluid me-2 mb-1 rounded' style={{ width: '30px', height: '30px' }} />
-					책잇아웃
-				</Navbar.Brand>
-				<Navbar.Toggle onClick={() => setExpanded(!expanded)}></Navbar.Toggle>
+		<>
+			{
+				<Card
+					className={`position-absolute d-inline d-lg-none p-0 ${initialLoad ? '' : showSearchBar ? 'search-bar-up' : 'search-bar-down'}`}
+					style={{ left: '5%', width: '90%', zIndex: '900', top: showSearchBar ? '75px' : '-10px' }}>
+					<Card.Body>
+						<SearchBar />
+					</Card.Body>
+				</Card>
+			}
 
-				<Navbar.Collapse id='responsive-navbar-nav'>
-					<Nav className='me-auto text-center'>
-						{uiSettings.topnav.link.map((url) => (
-							<Nav.Link
-								href={token !== '' && url.url}
-								active={location.pathname.startsWith(url.activeUrl)}
-								onClick={() => {
-									token === '' && toast.error('로그인 해 주세요')
-								}}>
-								{url.title}
-							</Nav.Link>
-						))}
-					</Nav>
+			<Navbar key={expand} expand={expand} fixed='top' bg='light' collapseOnSelect expanded={expanded}>
+				<Container fluid style={{ zIndex: '1000' }}>
+					<Navbar.Brand href={token === '' ? '/login' : '/'}>
+						<img src={logo} alt='' className='image-fluid me-2 mb-1 rounded' style={{ width: '30px', height: '30px' }} />
+						책잇아웃
+					</Navbar.Brand>
 
-					<Nav className={`d-inline d-${expand}-none text-center`}>
-						<Nav.Link href='/login' onClick={(e) => token !== '' && token != null && handleLogout(e)}>
-							<div className='row justify-content-center'>
-								<div className='col-4 col-md-2'>
-									<div className='row'>
-										<div className='col-4'>
-											<LoginIcon className='me-2 ' />
-										</div>
+					<span className='d-lg-none text-center'>
+						<SearchIcon className='h1 mt-2 me-5 text-secondary button-hover' onClick={() => {
+							setInitialLoad(false)
+							setShowSearchBar(!showSearchBar)
+						}} />
+					</span>
 
-										{token === '' || token == null ? (
-											<div className='col-xs-10 col-8'>로그인</div>
-										) : (
-											<div className='col-xs-10 col-8'>로그아웃</div>
-										)}
-									</div>
-								</div>
-							</div>
-						</Nav.Link>
+					<Navbar.Toggle onClick={() => setExpanded(!expanded)}></Navbar.Toggle>
 
-						{(token === '' || token == null) && (
-							<Nav.Link href='/join'>
+					<Navbar.Collapse id='responsive-navbar-nav'>
+						<Nav className='me-auto text-center'>
+							{uiSettings.topnav.link.map((url) => (
+								<Nav.Link
+									href={token !== '' && url.url}
+									active={location.pathname.startsWith(url.activeUrl)}
+									onClick={() => {
+										token === '' && toast.error('로그인 해 주세요')
+									}}>
+									{url.title}
+								</Nav.Link>
+							))}
+						</Nav>
+
+						<Nav className={`d-inline d-${expand}-none text-center`}>
+							<Nav.Link href='/login' onClick={(e) => token !== '' && token != null && handleLogout(e)}>
 								<div className='row justify-content-center'>
-									<div className='col-4  col-md-2'>
+									<div className='col-4 col-md-2'>
 										<div className='row'>
 											<div className='col-4'>
-												<JoinIcon className='me-2 ' />
+												<LoginIcon className='me-2 ' />
 											</div>
 
-											<div className='col-xs-10 col-8'>회원가입</div>
+											{token === '' || token == null ? (
+												<div className='col-xs-10 col-8'>로그인</div>
+											) : (
+												<div className='col-xs-10 col-8'>로그아웃</div>
+											)}
 										</div>
 									</div>
 								</div>
 							</Nav.Link>
-						)}
 
-						{(token !== '' || token == null) && (
-							<Nav.Link href='/settings'>
-								<div className='row justify-content-center'>
-									<div className='col-3 col-md-2'>
-										<div className='row'>
-											<div className='col-4'>
-												<SettingIcon className='me-2' />
+							{(token === '' || token == null) && (
+								<Nav.Link href='/join'>
+									<div className='row justify-content-center'>
+										<div className='col-4  col-md-2'>
+											<div className='row'>
+												<div className='col-4'>
+													<JoinIcon className='me-2 ' />
+												</div>
+
+												<div className='col-xs-10 col-8'>회원가입</div>
 											</div>
-
-											<div className='col-xs-10 col-8'>설정</div>
 										</div>
 									</div>
-								</div>
-							</Nav.Link>
-						)}
-					</Nav>
-
-					{token !== '' && token != null && <SearchBar />}
-
-					<Nav>
-						<NavDropdown
-							id='user-dropdown'
-							title={
-								<img
-									src={
-										localStorage.getItem('profile-image') == null || localStorage.getItem('profile-image') === ''
-											? userIcon
-											: localStorage.getItem('profile-image')
-									}
-									alt=''
-									className='img-fluid rounded'
-									style={{ width: '30px' }}
-								/>
-							}
-							align='end'
-							size='xl'
-							className={`d-none d-${expand}-inline`}>
-							{token === '' || token == null ? (
-								<>
-									<NavDropdown.Item href='/login'>로그인</NavDropdown.Item>
-									<NavDropdown.Item href='/join'>회원가입</NavDropdown.Item>
-								</>
-							) : (
-								<>
-									<NavDropdown.Item onClick={(e) => handleLogout(e)}>로그아웃</NavDropdown.Item>
-									<NavDropdown.Item onClick={() => navigate('/qna')}>QNA</NavDropdown.Item>
-									<NavDropdown.Item onClick={() => navigate('/faq')}>FAQ</NavDropdown.Item>
-									<NavDropdown.Divider />
-									<NavDropdown.Item href='/settings'>설정</NavDropdown.Item>
-								</>
+								</Nav.Link>
 							)}
-						</NavDropdown>
-					</Nav>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
+
+							{(token !== '' || token == null) && (
+								<Nav.Link href='/settings'>
+									<div className='row justify-content-center'>
+										<div className='col-3 col-md-2'>
+											<div className='row'>
+												<div className='col-4'>
+													<SettingIcon className='me-2' />
+												</div>
+
+												<div className='col-xs-10 col-8'>설정</div>
+											</div>
+										</div>
+									</div>
+								</Nav.Link>
+							)}
+						</Nav>
+
+						{token !== '' && token != null && (
+							<span className='d-none d-lg-inline'>
+								<SearchBar />
+							</span>
+						)}
+
+						<Nav>
+							<NavDropdown
+								id='user-dropdown'
+								title={
+									<img
+										src={
+											localStorage.getItem('profile-image') == null || localStorage.getItem('profile-image') === ''
+												? userIcon
+												: localStorage.getItem('profile-image')
+										}
+										alt=''
+										className='img-fluid rounded'
+										style={{ width: '30px', height: '30px' }}
+									/>
+								}
+								align='end'
+								size='xl'
+								className={`d-none d-${expand}-block`}>
+								{token === '' || token == null ? (
+									<>
+										<NavDropdown.Item href='/login'>로그인</NavDropdown.Item>
+										<NavDropdown.Item href='/join'>회원가입</NavDropdown.Item>
+									</>
+								) : (
+									<>
+										<NavDropdown.Item href='/qna'>QNA</NavDropdown.Item>
+										<NavDropdown.Item href='/faq'>FAQ</NavDropdown.Item>
+										<NavDropdown.Divider />
+										<NavDropdown.Item href='/settings'>설정</NavDropdown.Item>
+										<NavDropdown.Item onClick={(e) => handleLogout(e)}>로그아웃</NavDropdown.Item>
+									</>
+								)}
+							</NavDropdown>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
 	)
 }
 
-const SearchBar = ({ expand = 'lg' }) => {
+const SearchBar = () => {
 	const navigate = useNavigate()
 	const location = useLocation()
 
@@ -172,39 +204,39 @@ const SearchBar = ({ expand = 'lg' }) => {
 		}
 	}, [location])
 
-	const handleSearch = (keyword, e) => {
-		if (typeof e.target[0] !== 'undefined') {
-			e.target[0].blur()
-		}
+	const handleSearch = (e) => {
+		if (typeof e.target[0] !== 'undefined') e.target[0].blur()
 		e.preventDefault()
 
-		if (keyword.length >= 2) {
-			navigate(`/search/${keyword}`)
-		} else {
-			toast.error('2글자 이상의 검색어를 입력해 주세요')
-		}
+		if (keyword.length >= 2) navigate(`/search/${keyword}`)
+		else toast.error('2글자 이상의 검색어를 입력해 주세요')
 	}
 
 	return (
 		<Form
-			className='d-flex'
-			onSubmit={(e) => handleSearch(keyword, e)}
+			className='row me-1'
+			onSubmit={(e) => handleSearch(e)}
 			onKeyDown={(e) => {
 				if (e.keyCode === 13) this != null && this.blur()
 			}}>
-			<Form.Control
-				type='search'
-				placeholder='책 검색'
-				className='me-2'
-				aria-label='Search'
-				value={keyword}
-				onChange={(e) => {
-					setKeyword(e.target.value)
-				}}
-			/>
-			<Button type='submit' variant={`outline-success col-${expand}-3 col-2`}>
-				검색
-			</Button>
+			<div className='col-9 p-lg-0 pe-0'>
+				<Form.Control
+					type='search'
+					placeholder='책 검색'
+					className='me-2 w-100'
+					aria-label='Search'
+					value={keyword}
+					onChange={(e) => {
+						setKeyword(e.target.value)
+					}}
+				/>
+			</div>
+
+			<div className='col-3 pe-0'>
+				<Button type='submit' variant='outline-success' className='w-100'>
+					검색
+				</Button>
+			</div>
 		</Form>
 	)
 }
