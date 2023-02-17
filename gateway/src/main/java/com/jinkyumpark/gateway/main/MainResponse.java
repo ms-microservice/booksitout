2e922ed5547f0ee4590d7dab5aae396d9de728b8
jinkyumpark <jinpark1025@gmail.com>
2023-02-18 02:43:00 +0900

    core : move Info to gateway

--- gateway/src/main/java/com/jinkyumpark/gateway/main/MainResponse.java
+++ gateway/src/main/java/com/jinkyumpark/gateway/main/MainResponse.java
@@ -1,4 +1,4 @@
-package com.jinkyumpark.core.common.info;
+package com.jinkyumpark.gateway.main;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -6,12 +6,14 @@ import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Getter
-public class InfoResponse {
+public class MainResponse {
     private final LocalDateTime timestamp = LocalDateTime.now();
     private final String message;
+    private final String redirectUrl;
 
     @Builder
-    public InfoResponse(String message) {
+    public MainResponse(String message, String redirectUrl) {
         this.message = message;
+        this.redirectUrl = redirectUrl;
     }
 }
