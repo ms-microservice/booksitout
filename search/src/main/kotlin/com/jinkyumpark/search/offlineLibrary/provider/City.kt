bf77bf679eb17c5caf1c5e01d8eea9fcb650e51c
jinkyumpark <jinpark1025@gmail.com>
2023-04-18 14:19:13 +0900

    add library-related models
    - Region : for province level region in a country
    - City : for specific cities in the region
    - Library : for supported library
    - UserLibrary : for user specific settings

--- search/src/main/kotlin/com/jinkyumpark/search/offlineLibrary/provider/City.kt
+++ search/src/main/kotlin/com/jinkyumpark/search/offlineLibrary/provider/City.kt
@@ -1,35 +1,25 @@
 package com.jinkyumpark.search.offlineLibrary.provider
 
-enum class City(
-    val province: KoreaRegion,
-    val koreanName: String,
-    val apiRegionCode: Int,
-) {
+import com.jinkyumpark.search.offlineLibrary.provider.library.Library
+import javax.persistence.*
 
-    JONGNOGU(KoreaRegion.SEOUL, "종로구", 11010),
-    JUNGGU(KoreaRegion.SEOUL, "중구", 11020),
-    YONGSANGU(KoreaRegion.SEOUL, "용산구", 11030),
-    SEONGDONGGU(KoreaRegion.SEOUL, "성동구", 11040),
-    GWANGJINGU(KoreaRegion.SEOUL, "광진구", 11050),
-    DONGDAEMUNGU(KoreaRegion.SEOUL, "동대문구", 11060),
-    JUNGNANGGU(KoreaRegion.SEOUL, "중랑구", 11070),
-    SEONGBUKGU(KoreaRegion.SEOUL, "성북구", 11080),
-    GANGBUKGU(KoreaRegion.SEOUL, "강북구", 11090),
-    DOBONGGU(KoreaRegion.SEOUL, "도봉구", 11100),
-    NOWONGU(KoreaRegion.SEOUL, "노원구", 11110),
-    EUNPYEONGGU(KoreaRegion.SEOUL, "은평구", 11120),
-    SEODAEMUNGU(KoreaRegion.SEOUL, "서대문구", 11130),
-    MAPOGU(KoreaRegion.SEOUL, "마포구", 11140),
-    YANGCHEONGU(KoreaRegion.SEOUL, "양천구", 11150),
-    GANGSEOGU(KoreaRegion.SEOUL, "강서구", 11160),
-    GUROGU(KoreaRegion.SEOUL, "구로구", 11170),
-    GEUMCHEONGU(KoreaRegion.SEOUL, "금천구", 11180),
-    YEONGDEUNGPOGU(KoreaRegion.SEOUL, "영등포구", 11190),
-    DONGJAKGU(KoreaRegion.SEOUL, "동작구", 11200),
-    GWANAKGU(KoreaRegion.SEOUL, "관악구", 11210),
-    SEOCHOGU(KoreaRegion.SEOUL, "서초구", 11220),
-    GANGNAMGU(KoreaRegion.SEOUL, "강남구", 11230),
-    SONGPAGU(KoreaRegion.SEOUL, "송파구", 11240),
-    GANGDONGGU(KoreaRegion.SEOUL, "강동구", 11250);
+@Entity
+@Table(name = "CITY", uniqueConstraints = [
+    UniqueConstraint(name = "name_unique", columnNames = ["koreanName", "englishName"])
+])
+class City {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
 
+    var koreanName: String? = null
+    var englishName: String? = null
+
+    @ManyToOne @JoinColumn(name = "region_id", foreignKey = ForeignKey(name = "region_city_fk"))
+    var region: Region? = null
+
+    var dataLibraryApiCode: Int? = null
+    var logo: String? = null
+
+    @OneToMany(mappedBy = "city")
+    lateinit var libraries: List<Library>
 }
---
+++ search/src/main/kotlin/com/jinkyumpark/search/offlineLibrary/provider/library/UserLibrary.kt
@@ -0,0 +1,18 @@
+package com.jinkyumpark.search.offlineLibrary.provider.library
+
+import javax.persistence.*
+
+@Entity
+@Table(
+    uniqueConstraints = [
+        UniqueConstraint(name = "user_library_unique", columnNames = ["library_id", "appUserId"])
+    ]
+)
+class UserLibrary {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @ManyToOne @JoinColumn(name = "library_id", foreignKey = ForeignKey(name = "library_user_fk"))
+    var library: Library? = null
+    var appUserId: Long? = null
+}
