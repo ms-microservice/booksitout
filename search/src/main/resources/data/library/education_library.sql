0fbc6ac551a75ede4634b23397e04d94ff58a9ff
jinkyumpark <jinpark1025@gmail.com>
2023-04-21 18:47:47 +0900

    add education_library.sql

--- search/src/main/resources/data/library/education_library.sql
+++ search/src/main/resources/data/library/education_library.sql
@@ -1,65 +1,65 @@
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 고덕평생학습관', '', '', '', City.GANGDONGGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 고덕평생학습관', '서울 강동구 고덕로 295 고덕평생학습관', '05225', 'http://gdllc.sen.go.kr/', '', 25);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 노원평생학습관', '', '', '', City.NOWONGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 마포평생아현분관', '서울 노원구 동일로204길 13 노원평생학습관', '01783', 'http://nwllc.sen.go.kr/', '', 14);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 마포평생아현분관', '', '', '', City.MAPOGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 마포평생학습관', '서울 마포구 홍익로2길 16 마포평생학습관', '04054', 'http://mpllc.sen.go.kr/', '', 14);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 마포평생학습관', '', '', '', City.MAPOGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 영등포평생학습관', '서울 영등포구 국회대로53길 20 영동초등학교', '07229', 'http://ydpllc.sen.go.kr/', '', 19);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 영등포평생학습관', '', '', '', City.YEONGDEUNG);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 강남도서관', '서울 강남구 선릉로116길 45 서울특별시교육청 강남도서관', '06093', 'http://gnlib.sen.go.kr/', '', 23);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 강남도서관', '', '', '', City.GANGNAMGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 양천도서관', '서울 양천구 목동서로 113', '07988', 'http://yclib.sen.go.kr/', '', 15);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 양천도서관', '', '', '', City.YANGCHEONG);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 정독도서관', '서울 종로구 북촌로5길 48 정독도서관', '03055', 'http://jdlib.sen.go.kr/', '', 1);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 정독도서관', '', '', '', City.JONGNOGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 강동도서관', '서울 강동구 양재대로116길 57 강동도서관', '05344', 'http://gdlib.sen.go.kr/', '', 25);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 강동도서관', '', '', '', City.GANGDONGGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 고척도서관', '서울 구로구 고척로45길 39 구로구민체육센터', '08239', 'http://gclib.sen.go.kr/', '', 17);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 고척도서관', '', '', '', City.GUROGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 강서도서관', '서울 강서구 등촌로51나길 29 강서도서관', '07669', 'http://gslib.sen.go.kr/', '', 16);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 도봉도서관', '', '', '', City.DOBONGGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 구로도서관', '서울 구로구 공원로 15 시립구로도서관', '08298', 'http://grlib.sen.go.kr/', '', 17);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 어린이도서관', '', '', '', City.JONGNOGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 동대문도서관', '서울 동대문구 천호대로4길 22 동대문도서관', '02586', 'http://ddmlib.sen.go.kr/', '', 6);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 종로도서관', '', '', '', City.JONGNOGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 도봉도서관', '서울 도봉구 삼양로 556 도봉도서관', '01368', 'http://dblib.sen.go.kr/', '', 10);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 강서도서관', '', '', '', City.GANGSEOGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 서대문도서관', '서울 서대문구 모래내로 412 서대문도서관', '03717', 'https://sdmlib.sen.go.kr', '', 13);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 구로도서관', '', '', '', City.GUROGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 개포도서관', '서울 강남구 선릉로4길 30 개포도서관', '06327', 'http://gplib.sen.go.kr/', '', 23);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 동대문도서관', '', '', '', City.DONGDAEMUN);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 남삼도서관', '서울 용산구 소월로 109 남산도서관', '04340', 'http://nslib.sen.go.kr/', '', 3);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 서대문도서관', '', '', '', City.SEODAEMUNG);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 동작도서관', '서울 동작구 장승배기로 94 서울특별시립 동작도서관', '06925', 'http://djlib.sen.go.kr/', '', 20);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 개포도서관', '', '', '', City.SEOCHOGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 송파도서관', '서울 송파구 동남로 263', '05725', 'https://splib.sen.go.kr/', '', 24);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 남삼도서관', '', '', '', City.YONGSANGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 용산도서관', '서울 용산구 두텁바위로 160 용산도서관', '04328', 'http://yslib.sen.go.kr/', '', 3);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 동작도서관', '', '', '', City.DONGJAKGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 노원평생학습관', '서울 노원구 동일로204길 13 노원평생학습관', '01783', 'http://nwllc.sen.go.kr/', '', 11);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 송파도서관', '', '', '', City.SONGPAGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 어린이도서관', '서울 종로구 사직로9길 7 (사직동, 어린이도서관)', '03027', 'https://childlib.sen.go.kr/', '', 1);
 
-INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, region_id)
-VALUES ('', '서울시교육청 용산도서관', '', '', '', City.YONGSANGU);
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('', '서울시교육청 종로도서관', '서울 종로구 사직로9길 15-14 (사직동, 종로도서관)', '03027', 'https://jnlib.sen.go.kr/', '', 1);
