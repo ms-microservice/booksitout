6f0f9a501fa3b3ab10cd69471be99f243e89e023
jinkyumpark <jinpark1025@gmail.com>
2023-04-27 12:16:07 +0900

    search : update library data
    - remove json files
    - add 강남구, 동작구, 마포구
    - relocate

---
+++ search/src/main/resources/data/library/seoul/강남권/강남구.sql
@@ -0,0 +1,81 @@
+-- https://library.gangnam.go.kr/
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('DOGOKJEONGBOMUNHWADOSEOGWAN', '도곡정보문화도서관', '서울 강남구 도곡로18길 57 도곡정보문화도서관', '06269', 'http://dogoklib.gangnam.go.kr/', 'MA', 23); -- 02-451-1511
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('GAEPOHANEULKKUMDOSEOGWAN', '개포하늘꿈도서관', '서울 강남구 개포로110길 54', '06344', 'https://library.gangnam.go.kr/hnkklib', 'MM', 23); -- 02-451-1511
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('NONHYEONDOSEOGWAN', '논현도서관', '서울 강남구 학동로43길 17 논현2문화센터 6층', '06058', 'http://nhlib.gangnam.go.kr/', 'MB', 23); -- 02-3443-7650
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('NONHYEONJEONGBODOSEOGWAN', '논현정보도서관', '서울 강남구 학동로 169 3층', '06046', 'http://nhilib.gangnam.go.kr/', 'SA', 23); -- 02-511-7466
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('DAECHI1DONGJAGEUNDOSEOGWAN', '대치1동작은도서관', '서울 강남구 남부순환로391길 19 대치1문화센터 지하1층', '06280', 'http://dch1lib.gangnam.go.kr/', 'SB', 23); -- 02-3452-1170
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('DAECHIDOSEOGWAN', '대치도서관', '서울 강남구 삼성로 212 은마아파트 복지상가 2층 215호', '06284', 'http://dchlib.gangnam.go.kr/', 'MC', 23); -- 02-565-6666
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('MOSGOLDOSEOGWAN', '못골도서관', '서울 강남구 자곡로 116', '06373', 'http://mglib.gangnam.go.kr', 'MD', 23); -- 02-459-5522
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('MOSGOLHANOGEORINIDOSEOGWAN', '못골한옥어린이도서관', '서울 강남구 자곡로7길 3', '06372', 'http://hchildlib.gangnam.go.kr/', 'ME', 23); -- 02-2226-5930
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('DOGOK2DONGJUMINDOSEOGWAN', '도곡2동주민도서관', '서울 강남구 남부순환로378길 34-9 도곡2동주민센터', '06300', 'http://dg2lib.gangnam.go.kr/', 'TC', 23); -- 02-3423-7584
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('SEGOKDOSEOGWAN', '세곡도서관', '서울 강남구 밤고개로 286', '06377', 'http://sklib.gangnam.go.kr/', 'SD', 23); -- 02-445-0297
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('SEGOKMARUDOSEOGWAN', '세곡마루도서관', '서울 강남구 헌릉로590길 68', '06366', 'https://library.gangnam.go.kr/segokmarulib/index.do', 'SF', 23); -- 02-2176-0634
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('CHEONGDAMDOSEOGWAN', '청담도서관', '서울 강남구 압구정로79길 26 청담동주민센터', '06012', '', 'MK', 23);
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('JEULGEOUNDOSEOGWAN', '즐거운도서관', '서울 강남구 도곡로77길 23 대치4동주민센터', '06204', 'http://jgwlib.gangnam.go.kr/', 'MJ', 23); -- 02-565-7533
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('JEONGDAUNDOSEOGWAN', '정다운도서관', '서울 강남구 학동로67길 11 청담평생학습관 3층', '06066', 'http://jdwlib.gangnam.go.kr/', 'MI', 23); -- 02-512-9326
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('IRWONRAONYEONGEODOSEOGWAN', '일원라온영어도서관', '서울 강남구 영동대로 22 B1', '06346', 'https://library.gangnam.go.kr/englib/index.do', 'SH', 23); -- 02-6712-0595
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('YEOLLINDOSEOGWAN', '열린도서관', '서울 강남구 일원로 115 삼성생명빌딩 B동 203호', '06355', 'http://yllib.gangnam.go.kr/', 'MH', 23); -- 02-3412-3970
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('YEOKSAMPUREUNSOLDOSEOGWAN', '역삼푸른솔도서관', '서울 강남구 테헤란로8길 36 4층', '06240', 'http://ysplib.gangnam.go.kr/', 'MG', 23); -- 02-2051-1178
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('YEOKSAMDOSEOGWAN', '역삼도서관', '서울 강남구 역삼로7길 16 역삼1문화센터 5층', '06244', 'http://yslib.gangnam.go.kr/', 'MF', 23); -- 02-508-1139
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('YEOKSAM2DONGJAGEUNDOSEOGWAN', '역삼2동작은도서관', '서울 강남구 언주로 314 강남프라자 2층', '06219', 'http://ys2lib.gangnam.go.kr', 'SE', 23); -- 02-567-0930
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('HAENGBOKHANDOSEOGWAN', '행복한도서관', '서울 강남구 영동대로65길 24 대치2동주민센터 3층', '06187', 'http://happylib.gangnam.go.kr/', 'ML', 23); -- 02-567-3833
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('GAEPO4DONGJUMINDOSEOGWAN', '개포4동주민도서관', '서울 강남구 개포로24길 33 3층', '06308', 'http://gp4lib.gangnam.go.kr/', 'TD', 23); -- 02-3423-7884
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('SUSEODONGJUMINDOSEOGWAN', '수서동주민도서관', '서울 강남구 광평로 301-4 수서동주민센터2층', '06349', 'http://suseolib.gangnam.go.kr/', 'TG', 23); -- 02-3423-8584
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('SINSADONGJUMINDOSEOGWAN', '신사동주민도서관', '서울 강남구 압구정로 128', '06029', 'http://sinsalib.gangnam.go.kr/', 'TA', 23); -- 02-3423-7334
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('APGUJEONGDONGJUMINDOSEOGWAN', '압구정동주민도서관', '서울 강남구 압구정로33길 48 압구정동주민센터', '06004', '', 'TB', 23); -- 02-3443-6574
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('IRWON1DONGJUMINDOSEOGWAN', '일원1동주민도서관', '서울 강남구 양재대로55길 14', '06341', 'http://iw1lib.gangnam.go.kr/', 'TF', 23); -- 02-3423-8384
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('IRWONBONDONGJUMINDOSEOGWAN', '일원본동주민도서관', '서울 강남구 광평로 126', '06361', 'http://iblib.gangnam.go.kr/', 'TE', 23); -- 02-3423-8284
+
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id);
+VALUES ('DOGOK2DONGJUMINDOSEOGWAN', '도곡2동주민도서관', '서울 강남구 남부순환로378길 34-9 도곡2동주민센터', '06300', 'http://dg2lib.gangnam.go.kr/', 'TC', 23) -- 02-3423-7584
---
+++ search/src/main/resources/data/library/seoul/강서권/동작구.sql
@@ -0,0 +1,22 @@
+-- https://lib.dongjak.go.kr/
+-- https://lib.dongjak.go.kr/dj/intro/search/index.do?menu_idx=111&booktype=BOOK&libraryCodes=lib_ML&search_type=L_TITLE&search_text=%EC%9C%A0%EB%82%9C%ED%95%9C+%EB%8F%84%EC%A0%84
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('GIMYEONGSAMDOSEOGWAN', '김영삼도서관', '서울 동작구 매봉로 1', '06919', 'http://lib.dongjak.go.kr/dj/index.do', 'lib_MA', 20); -- 02-827-0557
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('SADANGSOLBATDOSEOGWAN', '사당솔밭도서관', '서울 동작구 솔밭로 86', '07019', 'http://lib.dongjak.go.kr', 'lib_MC', 20); -- 02-585-8411
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('DONGJAGEORINIDOSEOGWAN', '동작어린이도서관', '서울 동작구 장승배기로16길 98 동작어린이도서관', '06926', 'http://lib.dongjak.go.kr/', 'lib_Mt', 20); -- 02-823-6750
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('DAEBANGEORINIDOSEOGWAN', '대방어린이도서관', '서울 동작구 대방동길 55', '06945', 'http://lib.dongjak.go.kr/', 'lib_MF', 20); -- 02-813-6740
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('YAKSUDOSEOGWAN', '약수도서관', '서울 동작구 양녕로22바길 64', '07036', '', 'lib_ME', 20); -- 07036
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('DONGJAKSAEMTEODOSEOGWAN', '동작샘터도서관', '서울 동작구 동작대로29길 63-26 동작구다문화가족지원센터', '06999', 'http://lib.dongjak.go.kr/', 'lib_MH', 20); -- 02-533-6750
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('DAULJAGEUNDOSEOGWAN', '다울작은도서관', '서울 동작구 사당로20길 132', '07017', '', 'lib_MJ', 20); -- 02-3473-3360
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('GUKSABONGSUPSOKJAGEUNDOSEOGWAN', '국사봉숲속작은도서관', '서울 동작구 양녕로23길 27 국사봉숲속작은도서관', '07049', 'http://lib.dongjak.go.kr/dj/index.do', 'lib_MK', 20); -- 010-5849-2327
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('KKAMANGDOLDOSEOGWAN', '까망돌도서관', '서울 동작구 서달로 129', '서울 동작구 서달로 129', 'https://lib.dongjak.go.kr/dj/index.do', 'lib_Ml', 20); -- 02-815-3701
+INSERT INTO library (english_name, korean_name, address, post_code, link, web_crawling_code, city_id)
+VALUES ('SINDAEBANGNURIDOSEOGWAN ', '신대방누리도서관', '서울 동작구 신대방1다길 19', '07074', '', 'lib_MN', 20);
