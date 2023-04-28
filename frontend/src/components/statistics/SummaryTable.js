c1adecbfd2c5197ffd985cdb7fb806cf3fa4a090
jinkyumpark <jinpark1025@gmail.com>
2023-04-29 00:36:56 +0900

    frontend : modify SummaryTable.js images

--- frontend/src/components/statistics/SummaryTable.js
+++ frontend/src/components/statistics/SummaryTable.js
@@ -1,10 +1,21 @@
 // Icons
 import timeIcon from '../../resources/images/statistics/time.png'
-import bookIcon from '../../resources/images/statistics/book.png'
-import averageIcon from '../../resources/images/statistics/average.png'
+
+// import averageIcon from '../../resources/images/statistics/average.png'
+// import averageIcon from '../../resources/images/statistics/green-chart.png'
+import averageIcon from '../../resources/images/statistics/simple-chart.png'
+
+// import bookIcon from '../../resources/images/statistics/book.png'
+// import bookIcon from '../../resources/images/statistics/green-book.png'
+// import bookIcon from '../../resources/images/statistics/book-shelf.png'
+import bookIcon from '../../resources/images/statistics/book-overlap.png'
+
 import starIcon from '../../resources/images/statistics/star.png'
+
 import longestDayIcon from '../../resources/images/statistics/bookworm.png'
+
 import pageIcon from '../../resources/images/statistics/page.png'
+// import pageIcon from '../../resources/images/statistics/page-simple.png'
 
 const SummaryTable = ({ statistics }) => {
 	const statisticsData = [
