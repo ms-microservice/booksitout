b86a07ee6fca2595fa812dc0a894126f635adb82
jinkyumpark <jinpark1025@gmail.com>
2023-04-29 13:06:42 +0900

    search : add validation in build.gradle.kts

--- search/build.gradle.kts
+++ search/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
