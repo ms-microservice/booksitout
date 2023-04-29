8c24697855c26030cbcc9ad7bd79ddebca00d22b
jinkyumpark <jinpark1025@gmail.com>
2023-04-29 12:58:59 +0900

    search : modify ApiGoogleImageSearchResponse fields to nullable

--- search/src/main/kotlin/com/jinkyumpark/search/apiResponse/google/ApiGoogleImageSearchResponse.kt
+++ search/src/main/kotlin/com/jinkyumpark/search/apiResponse/google/ApiGoogleImageSearchResponse.kt
@@ -10,71 +10,71 @@ data class ApiGoogleImageSearchResponse(
 )
 
 data class ApiGoogleUrl(
-    val type: String,
-    val template: String,
+    val type: String?,
+    val template: String?,
 )
 
 data class ApiGoogleQueries(
-    val request: List<ApiGoogleRequest>,
-    val nextPage: List<ApiGoogleNextPage>,
+    val request: List<ApiGoogleRequest?>,
+    val nextPage: List<ApiGoogleNextPage?>,
 )
 
 data class ApiGoogleRequest(
-    val title: String,
-    val totalResults: String,
-    val searchTerms: String,
-    val count: Int,
-    val startIndex: Int,
-    val inputEncoding: String,
-    val outputEncoding: String,
-    val safe: String,
-    val cx: String,
-    val searchType: String,
+    val title: String?,
+    val totalResults: String?,
+    val searchTerms: String?,
+    val count: Int?,
+    val startIndex: Int?,
+    val inputEncoding: String?,
+    val outputEncoding: String?,
+    val safe: String?,
+    val cx: String?,
+    val searchType: String?,
 )
 
 data class ApiGoogleNextPage(
-    val title: String,
-    val totalResults: String,
-    val searchTerms: String,
-    val count: Int,
-    val startIndex: Int,
-    val inputEncoding: String,
-    val outputEncoding: String,
-    val safe: String,
-    val cx: String,
-    val searchType: String,
+    val title: String?,
+    val totalResults: String?,
+    val searchTerms: String?,
+    val count: Int?,
+    val startIndex: Int?,
+    val inputEncoding: String?,
+    val outputEncoding: String?,
+    val safe: String?,
+    val cx: String?,
+    val searchType: String?,
 )
 
 data class ApiGoogleContext(
-    val title: String,
+    val title: String?,
 )
 
 data class ApiGoogleSearchInformation(
-    val searchTime: Double,
-    val formattedSearchTime: String,
-    val totalResults: String,
-    val formattedTotalResults: String,
+    val searchTime: Double?,
+    val formattedSearchTime: String?,
+    val totalResults: String?,
+    val formattedTotalResults: String?,
 )
 
 data class ApiGoogleItem(
-    val kind: String,
-    val title: String,
-    val htmlTitle: String,
-    val link: String,
-    val displayLink: String,
-    val snippet: String,
-    val htmlSnippet: String,
-    val mime: String,
-    val fileFormat: String,
-    val image: ApiGoogleItemImage,
+    val kind: String?,
+    val title: String?,
+    val htmlTitle: String?,
+    val link: String?,
+    val displayLink: String?,
+    val snippet: String?,
+    val htmlSnippet: String?,
+    val mime: String?,
+    val fileFormat: String?,
+    val image: ApiGoogleItemImage?,
 )
 
 data class ApiGoogleItemImage(
-    val contextLink: String,
-    val height: Int,
-    val width: Int,
-    val byteSize: Int,
-    val thumbnailLink: String,
-    val thumbnailHeight: Int,
-    val thumbnailWidth: Int,
+    val contextLink: String?,
+    val height: Int?,
+    val width: Int?,
+    val byteSize: Int?,
+    val thumbnailLink: String?,
+    val thumbnailHeight: Int?,
+    val thumbnailWidth: Int?,
 )
