c3d4b9e8d433605abad554f2af212f80729c9a3c
jinkyumpark <jinpark1025@gmail.com>
2023-05-15 00:05:02 +0900

    core : add feign client for search

--- core/src/main/java/com/jinkyumpark/core/common/feign/OpenFeignConfig.java
+++ core/src/main/java/com/jinkyumpark/core/common/feign/OpenFeignConfig.java
@@ -1,4 +1,4 @@
-package com.jinkyumpark.forum.feign;
+package com.jinkyumpark.core.common.feign;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
---
+++ core/src/main/java/com/jinkyumpark/core/common/feign/SearchClient.java
@@ -0,0 +1,16 @@
+package com.jinkyumpark.core.common.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "searchNew", url = "${feign.url.search}")
+public interface SearchClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "v3/search/new/naver")
+    List<NewBookSearchResponse> getNewBookSearchResultFromNaver(@RequestParam("query") String query);
+
+}
