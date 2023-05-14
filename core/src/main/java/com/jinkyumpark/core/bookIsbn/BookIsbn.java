a4cd4b7d380355941d143bf04051fd0d522224a8
jinkyumpark <jinpark1025@gmail.com>
2023-05-15 00:19:12 +0900

    core : add BookIsbn

--- core/src/main/java/com/jinkyumpark/core/bookIsbn/BookIsbn.java
+++ core/src/main/java/com/jinkyumpark/core/bookIsbn/BookIsbn.java
@@ -1,5 +1,6 @@
 package com.jinkyumpark.core.bookIsbn;
 
+import com.jinkyumpark.core.common.feign.NewBookSearchResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,16 +9,35 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Getter
-@NoArgsConstructor @AllArgsConstructor @Builder
-@Entity @Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(name = "book_isbn_unique", columnNames = {"isbn13"})})
 public class BookIsbn {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookIsbnId;
 
-    private Integer isbn13;
+    @Column(unique = true)
+    private Long isbn13;
+
     private String title;
     private String author;
     private String cover;
+
+
+    public static BookIsbn of(NewBookSearchResponse newBookSearchResponse) {
+        int index = newBookSearchResponse.getTitle().lastIndexOf('(') == -1 ? newBookSearchResponse.getTitle().length() : newBookSearchResponse.getTitle().lastIndexOf('(');
+
+        return BookIsbn.builder()
+                .isbn13(Long.parseLong(newBookSearchResponse.getIsbn()))
+                .title(newBookSearchResponse.getTitle().substring(0, index))
+                .author(newBookSearchResponse.getAuthor())
+                .cover(newBookSearchResponse.getCover())
+                .build();
+    }
 
 }
