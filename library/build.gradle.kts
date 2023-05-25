d2b1a225df6ef8e9fc98a337ad54e05deebd2f56
jinkyumpark <jinpark1025@gmail.com>
2023-05-26 02:26:03 +0900

    library : add library module
    - import common module
    - add necessary dependencies

---
+++ library/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+tasks.register("prepareKotlinBuildScriptModel") {}
+plugins {
+    kotlin("jvm") version "1.5.30"
+    kotlin("plugin.spring") version "1.5.30"
+}
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    testImplementation("io.projectreactor:reactor-test")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.+")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+    implementation("org.springframework.cloud:spring-cloud-starter-zipkin:2.2.8.RELEASE")
+
+    developmentOnly("io.netty:netty-resolver-dns-native-macos:4.1.75.Final") {
+        artifact { classifier = "osx-aarch_64" }
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
