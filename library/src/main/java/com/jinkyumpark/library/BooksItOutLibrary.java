d170d557d42abe719c551f1fd13cf88460cbb7d0
jinkyumpark <jinpark1025@gmail.com>
2023-05-26 15:57:10 +0900

    library : fix bugs
    - add batch dependency
    - add main class
    - modify application name in properties

---
+++ library/src/main/java/com/jinkyumpark/library/BooksItOutLibrary.java
@@ -0,0 +1,13 @@
+package com.jinkyumpark.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksItOutLibrary {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksItOutLibrary.class, args);
+    }
+
+}
