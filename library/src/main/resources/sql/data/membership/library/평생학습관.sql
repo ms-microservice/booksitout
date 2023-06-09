df9752ff57572867d8ae6062244548046ccc6914
jinkyumpark <jinpark1025@gmail.com>
2023-06-09 17:01:13 +0900

    library : add membership available library data (영등포구립, 평생학습관)

---
+++ library/src/main/resources/sql/data/membership/library/평생학습관.sql
@@ -0,0 +1,83 @@
+# 남산도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 653);
+
+# 서울특별시교육청정독도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 673);
+
+# 서울특별시교육청마포평생학습관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 658);
+
+# 서울특별시교육청노원평생학습관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 654);
+
+# 서울특별시교육청영등포평생학습관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 671);
+
+# 서울특별시교육청고덕평생학습관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 65);
+
+# 서울특별시교육청양천도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 670);
+
+# 서울특별시교육청서울시립어린이도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 668);
+
+# 서울특별시교육청용산도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 672);
+
+# 서울특별시교육청종로도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 674);
+
+# 서울특별시교육청강남도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 646);
+
+# 서울특별시교육청강동도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 647);
+
+# 서울특별시교육청강서도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 648);
+
+# 서울특별시교육청개포도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 649);
+
+# 서울특별시교육청고척도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 651);
+
+# 서울특별시교육청구로도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 652);
+
+# 서울특별시교육청동대문도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 656);
+
+# 서울특별시교육청동작도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 657);
+
+# 서울특별시교육청마포평생학습관아현분관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 659);
+
+# 서울특별시교육청서대문도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 66);
+
+# 서울특별시교육청송파도서관
+insert into library.membership_type_library (library.membership_type_library.membership_type_id, membership_type_library.library_id)
+values (2, 669);
